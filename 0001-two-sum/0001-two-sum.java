77e5eb5a298479a8752f89368aac83333190ffc2
Sumit yogi <yogisumit97@gmail.com>
2025-04-01 10:18:31 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -8,11 +8,4 @@ class Solution {
         }
         return new int[] {-1,-1};
     }
-    public static void main(String[] args) {
-    Solution solution = new Solution();
-    int[] nums = {2, 7, 11, 15};
-    int target = 9;
-    int[] result = solution.twoSum(nums, target);
-    System.out.println("Result: [" + result[0] + ", " + result[1] + "]");
-}
 }
