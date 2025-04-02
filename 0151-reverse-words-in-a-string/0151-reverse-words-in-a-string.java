bb44a7cdb400acb44669ec71f2ea7f28342c7ebb
Sumit yogi <yogisumit97@gmail.com>
2025-04-02 10:54:21 +0530

    Update 0151-reverse-words-in-a-string.java
    Updated comments

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,11 +1,11 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" +");
+        String[] words = s.split(" +"); //splitting string into strings using regex
         StringBuilder ans = new StringBuilder();
         for(int i=words.length-1; i>=0;i--){
             ans.append(words[i]);
             ans.append(' ');
         }
-        return ans.toString().trim();
+        return ans.toString().trim(); // trim removes leading and trailing whitespaces
     }
 }
