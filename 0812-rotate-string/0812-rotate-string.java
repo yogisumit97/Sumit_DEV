dc9a362fc6adb8cdb735d91d7e4634825cb216ef
Sumit yogi <yogisumit97@gmail.com>
2025-04-02 12:07:15 +0530

    Time: 1 ms (19.77%), Space: 42.9 MB (5.45%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -2,19 +2,17 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         StringBuilder sb = new StringBuilder();
         while(!s.equals(sb.toString())){
-            if(sb.length()==0){
+            if(sb.length()==0){  // start sb from 1 time shift of s
                 sb.append(s.substring(1,s.length()));
                 sb.append(s.charAt(0));
-                //System.out.println(sb);
             }
             else{
                 String str = sb.toString();
-                sb.delete(0,sb.length());
+                sb.delete(0,sb.length()); //keep shifting sb till it again becomes s
                 sb.append(str.substring(1,str.length()));
                 sb.append(str.charAt(0));
-                //System.out.println(sb);
             }
-            if(goal.equals(sb.toString())) return true;
+            if(goal.equals(sb.toString())) return true; // if while shifting goal is reached return true
         }
         return false;
     }
