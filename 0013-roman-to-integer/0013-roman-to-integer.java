1ee68251a1cbb039ecd7feb8df8ff2a1fdb79ca3
Sumit yogi <yogisumit97@gmail.com>
2025-04-02 12:42:38 +0530

    Time: 2 ms (100%), Space: 44.9 MB (36.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        int op=0;
-        int len = s.length();
-        //System.out.println(len);
-        for(int i=0; i<len; i++){
-            char ch= s.charAt(i);
-            if(ch == 'M' && i!=0 && (s.charAt(i-1)=='C')){
-                op+=800;
+        int ans = 0;
+        for(int i= s.length()-1; i>=0; i--){
+            int val=0;
+            switch(s.charAt(i)){
+                case 'I' : val =1; break;
+                case 'V' : val =5; break;
+                case 'X' : val =10; break;
+                case 'L' : val =50; break;
+                case 'C' : val =100; break;
+                case 'D' : val =500; break;
+                case 'M' : val =1000; break;
             }
-            else if(ch == 'M') op+=1000;
-            else if(ch == 'D' && i!=0 && (s.charAt(i-1)=='C')){
-                op+=300;
-            }
-            else if(ch == 'D') op+=500;
-            else if(ch == 'C' && i!=0 && (s.charAt(i-1)=='X')){
-                op+=80;
-            }
-            else if(ch == 'C') op+=100;
-            else if(ch == 'L' && i!=0 && (s.charAt(i-1)=='X')){
-                op+=30;
-            }
-            else if(ch == 'L') op+=50;
-            else if(ch == 'X' && i!=0 && (s.charAt(i-1)=='I')){
-                op+=8;
-            }
-            else if(ch == 'X') op+=10;
-            else if(ch == 'V' && i!=0 && (s.charAt(i-1)=='I')){
-                op+=3;
-            }
-            else if(ch == 'V') op+=5;
-            else op+=1;            
+            if(4*val<ans) ans-=val;
+            else ans+=val;
         }
-        return op;
+        return ans;
     }
 }
