cc67ef1f4bc949fab17544774392ba21a52982cb
Sumit yogi <yogisumit97@gmail.com>
2025-04-04 14:48:17 +0530

    Time: 17 ms (33.34%), Space: 45.7 MB (51.47%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -3,11 +3,11 @@ class Solution {
         HashMap<Character,Integer> hm = new HashMap<>(); // hashMap with freq
         for(int i =0; i<s.length();i++) hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0)+1);
         StringBuilder ans = new StringBuilder();
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(hm.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-        list.forEach( (point) -> { 
-            for(int i =point.getValue()-1; i>=0;i--) ans.append(point.getKey());
-         } );
-        return ans.reverse().toString();
+        ArrayList<Character> list = new ArrayList(hm.keySet());
+        list.sort((ob1,ob2) -> hm.get(ob2)-hm.get(ob1));  //decresing order as ob2-ob1 checked for ob1,ob2
+        for(char ch : list){
+            for(int i =0; i<hm.get(ch); i++) ans.append(ch);
+        }
+        return ans.toString();
 	}
 }
