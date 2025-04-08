6838b499d23b33d842f54aaa5aac736807087957
Sumit yogi <yogisumit97@gmail.com>
2025-04-08 16:11:58 +0530

    Time: 1 ms (100%), Space: 44.5 MB (52.92%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,57 +10,46 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode curr = head;
-        int mask = 0;
-        while(!(l1==null && l2==null)){
-            if(l1==null){
-                curr.next = l2;
-                curr = curr.next;
-                curr.val = curr.val + mask;
-                mask = curr.val/10;
-                curr.val = curr.val%10;
-                l2 = l2.next;
-                if(l2==null && mask!=0){
-                    ListNode temp = new ListNode(mask,null);
-                    curr.next =temp;
-                }
-                continue;
-            } else if(l2==null){
-                curr.next = l1;
-                curr = curr.next;
-                curr.val = curr.val + mask;
-                mask = curr.val/10;
-                curr.val = curr.val%10;
-                l1 = l1.next;
-                if(l1==null && mask!=0){
-                    ListNode temp = new ListNode(mask,null);
-                    curr.next =temp;
-                }
-                continue;
+        ListNode head = null;
+        ListNode curr = null;
+        int carry = 0;
+        while(l1!=null && l2!=null){
+            int num = carry+l1.val+l2.val;
+            carry = num/10;
+            num = num%10;
+            ListNode temp = new ListNode(num);
+            if(head==null){
+                head = temp;
+                curr = head;
             }
             else{
-                ListNode temp = new ListNode(0,null);
-                int num = l1.val + l2.val+ mask;
-                l1 = l1.next;
-                l2 = l2.next;
-                mask = num/10;
-                num = num%10;
-                temp.val = num;
-                if(head==null){
-                    head = temp;
-                    curr=temp;
-                    continue;
-                }
-                
                 curr.next = temp;
                 curr = curr.next;
-                if(l1==null && l2==null && mask!=0){
-                    ListNode temp2 = new ListNode(mask,null);
-                    curr.next = temp2;
-                }
             }
+            l1= l1.next;
+            l2 = l2.next;
+        }   
+        while(l1!=null){
+            int num = carry+l1.val;
+            carry = num/10;
+            num = num%10;
+            ListNode temp = new ListNode(num);
+            curr.next = temp;
+            curr = curr.next;
+            l1 = l1.next;
+        } 
+        while(l2!=null){
+            int num = carry+l2.val;
+            carry = num/10;
+            num = num%10;
+            ListNode temp = new ListNode(num);
+            curr.next = temp;
+            curr = curr.next;
+            l2 = l2.next;
         }
-        return head.next;
+        if(carry!=0){
+            curr.next = new ListNode(carry);
+        }
+        return head;
     }
 }
