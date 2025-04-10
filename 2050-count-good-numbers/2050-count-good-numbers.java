fbb99791f86c480d96e60160b0974717697e2a71
Sumit yogi <yogisumit97@gmail.com>
2025-04-10 15:29:23 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.92%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    static final int MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+        long even = (n+1)/2;
+        long odd = n/2;
+
+        long resEven = power(5, even);
+        long resOdd = power(4, odd);
+
+        return (int)((resEven*resOdd)%MOD);
+    }
+    public long power(int base, long pow){
+        if(pow==0) return 1;
+        long half = power(base, pow/2);
+        long result = (half*half)%MOD;
+        if(pow%2==1) result = (result*base)%MOD;
+        return result;
+    }
+}
