4cc844cae6eb402e1f9f297a29baf2b3a38fa5fc
Sumit yogi <yogisumit97@gmail.com>
2025-04-10 16:13:33 +0530

    Time: 2 ms (28.74%), Space: 42.5 MB (65.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
+        String temp = s.trim();
+        if(temp.length()==0) return 0;
         int sign = 1;
-        int ans = 0;
-        int oper = 0;
-        for(int i =0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c==' ' && oper ==0) continue;
-            else if(c=='-' && oper ==0){
-                sign = -1; oper=1;
-            } 
-            else if(c=='+' && oper==0){
-                sign = 1; oper =1;
-            }
-            else if(!Character.isDigit(c)) break;
-            else{
-                oper = 1;
-                if(ans>Integer.MAX_VALUE/10){
-                    if(sign<0) return Integer.MIN_VALUE;
-                    else return Integer.MAX_VALUE;
-                }
-                else if(ans<Integer.MIN_VALUE/10){
-                    if(sign<0) return Integer.MIN_VALUE;
-                    else return Integer.MAX_VALUE;
-                } 
-                ans = ans*10+ Character.getNumericValue(c);
-                if(ans<0 && sign ==1) return Integer.MAX_VALUE;
-                else if(ans<0 && sign ==-1) return Integer.MIN_VALUE;
-            }
+        if(temp.charAt(0)=='-'){
+            temp = temp.substring(1,temp.length());
+            sign = -1;
         }
-        return sign*ans;
+        else if(temp.charAt(0)=='+') temp = temp.substring(1,temp.length());
+        return getNumber(temp,sign);
+    }
+    public int getNumber(String s, int sign){
+        int num = 0;
+        for(int i =0;i<s.length();i++){
+            if(!Character.isDigit(s.charAt(i))) break;
+            int digit = (s.charAt(i)-'0');
+            // Check for overflow
+            if (num > Integer.MAX_VALUE / 10 || 
+                (num == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            num = num*10+ digit;
+        }
+        return sign*num;
     }
 }
