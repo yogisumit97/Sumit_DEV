b294a0969b189d117bca44351f45bc4ad03c8fea
Sumit yogi <yogisumit97@gmail.com>
2025-04-11 10:26:20 +0530

    Time: 1 ms (26.08%), Space: 40.9 MB (61.36%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1) return true;
+        else if(n<=0 || n%4!=0) return false;
+        else return isPowerOfFour(n/4);
+    }
+}
