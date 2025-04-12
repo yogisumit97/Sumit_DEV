78edba2889ecfb8095c3990d17cd07617ff5deca
Sumit yogi <yogisumit97@gmail.com>
2025-04-12 10:53:00 +0530

    Time: 2 ms (82.78%), Space: 44.7 MB (64.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,18 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        return findSubset(0, candidates, result,0, ans, target);
-    }
-    public List<List<Integer>> findSubset(int index,int[] nums, List<Integer> result,int sum, List<List<Integer>> ans, int target){
-        if(index >= nums.length || sum>=target){ //base case
-            if(sum==target) ans.add(new ArrayList<>(result)); //as java passes reference, take new 
-            return ans;
-        }
-
-        //take or not take method
-        result.add(nums[index]); //take
-        sum+=nums[index];
-        findSubset(index, nums, result,sum, ans,target);
-        result.remove(result.size()-1); //not take
-        sum-=nums[index];
-        findSubset(index+1, nums, result,sum, ans, target);
+        generateCombinationsHelper(0,candidates,target,0, new ArrayList<Integer>(), ans);
         return ans;
     }
+    public void generateCombinationsHelper(int index,int[] arr, int target,int sum, List<Integer> result, List<List<Integer>> ans){
+        if(index>=arr.length || sum>=target){
+            if(sum==target) ans.add(new ArrayList<Integer>(result));    
+            return;
+        }
+        result.add(arr[index]);//take element
+        generateCombinationsHelper(index,arr,target,sum+arr[index], result, ans);
+        result.remove(result.size()-1);//not take element
+        generateCombinationsHelper(index+1,arr,target,sum, result, ans);
+        return;
+    }
 }
-
-
-
