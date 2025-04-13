a3b5f1ade98ec47c99367fdedc915f0be55ae5b3
Sumit yogi <yogisumit97@gmail.com>
2025-04-13 12:38:36 +0530

    Time: 3 ms (48.52%), Space: 44.9 MB (75.47%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,50 +1,44 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n]; //board[i][j] = 0 means it is empty space and it can be filled with queen
+        char[][] board = new char[n][n]; //board[i][j] = '.' means it is empty space and it can be filled with queen
+        for(int l = 0; l<board.length; l++){
+            for(int r=0; r<board.length; r++) board[l][r] = '.';
+        }
         List<List<String>> ans = new ArrayList<>();
-        nextQueenPositionHelper(board, 0, ans);
+        nextQueenPositionHelper(board, 0, ans); 
         return ans;    
     }
-    private void nextQueenPositionHelper(char[][] board, int j, List<List<String>> ans){
-        if(j>=board.length){
+    private void nextQueenPositionHelper(char[][] board, int col, List<List<String>> ans){
+        if(col>=board.length){
             List<String> result = new ArrayList<>();
-            for(int i =0; i<board.length;i++){
-                result.add(new String(board[i]));
-            }
+            for(int i =0; i<board.length;i++) result.add(new String(board[i]));
             ans.add(new ArrayList<>(result));
             return;
         }
         for(int i=0; i<board.length;i++){ //fill in first row and check further
-            if(board[i][j]=='.') continue; //already attacked position
-            char[][] temp = new char[board.length][board.length];
-            for(int l = 0; l<board.length; l++){
-                for(int r=0; r<board.length; r++) temp[l][r] = board[l][r];
-            }
-            markBoardAttacked(board, i, j, board.length);
-            board[i][j] ='Q'; // place queen at i,j
-            nextQueenPositionHelper(board, j+1, ans);
-            for(int l = 0; l<board.length; l++){  //unmark attacked positions.
-                for(int r=0; r<board.length; r++) board[l][r] = temp[l][r];
-            }
+            if(!checkQueenPossibility(i,col, board,board.length)) continue;
+            board[i][col] ='Q'; // place queen at i,col.
+            nextQueenPositionHelper(board, col+1, ans);
+            board[i][col] ='.'; // replace for backtracking.
         }
-        return;
     }
-    private void markBoardAttacked(char[][] board, int i, int j, int n){ //queen at i,j position, mark relevant pos as '.'
-        for(int k = 0; k<n; k++) board[i][k] = '.';// mark horizontally.
-        for(int k = 0; k<n; k++) board[k][j] = '.';// mark vertically. 
-        int l= i;
-        int r= j;
-        while(l>=0 && r>=0 && l<n && r<n){// mark diagonally downwards.
-            board[l][r] = '.';
-            l = l+1;
-            r = r+1;
+    private boolean checkQueenPossibility(int row, int col, char[][] board, int n){ //queen at row,col position
+        for(int j = 0; j<col; j++) if(board[row][j]=='Q') return false;// check horizontally.
+        for(int i = 0; i<row; i++) if(board[i][col]=='Q') return false;// check vertically. 
+        int i= row;
+        int j= col;
+        while(i>=0 && j>=0 && i<n && j<n){// check diagonally downwards.
+            if(board[i][j]=='Q') return false;
+            i = i+1;
+            j = j-1;
         }
-        l= i;
-        r= j;
-        while(l>=0 && r>=0 && l<n && r<n){// mark diagonally downwards.
-            board[l][r] = '.';
-            l = l-1;
-            r = r+1;
+        i= row;
+        j= col;
+        while(i>=0 && j>=0 && i<n && j<n){// check diagonally upwards.
+            if(board[i][j]=='Q') return false;
+            i = i-1;
+            j = j-1;
         }
+        return true;
     }
 }
