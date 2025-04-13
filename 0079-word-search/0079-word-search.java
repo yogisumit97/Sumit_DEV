c14f5c737259a1785f38c54f0fc6433b7d710c98
Sumit yogi <yogisumit97@gmail.com>
2025-04-13 11:11:02 +0530

    Time: 141 ms (64.74%), Space: 41.6 MB (62.92%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -4,23 +4,22 @@ class Solution {
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
                 if(board[i][j]==word.charAt(0)){
-                    ans = ans || wordSearchHelper(i, j, board.length, board[0].length, board, word);
+                    ans = ans || wordSearchHelper(i, j, board.length, board[0].length, board, word, 0);
                     if(ans== true) return ans;
                 }
             }
         }
         return false;
     }
-    private boolean wordSearchHelper(int i, int j, int row, int col, char[][] board, String word){
-        if(i<0||j<0||i>=row||j>= col||board[i][j]=='-'|| board[i][j]!=word.charAt(0)) return false;
-        if(word.length()==0) return true;
-        if(word.length()==1 && board[i][j]==word.charAt(0)) return true;
+    private boolean wordSearchHelper(int i, int j, int row, int col, char[][] board, String word, int index){
+        if(index == word.length()) return true;
+        if(i<0||j<0||i>=row||j>= col||board[i][j]!=word.charAt(index)) return false;
         char temp = board[i][j];
         board[i][j] = '-';
-        if(wordSearchHelper(i, j-1, board.length, board[0].length, board, word.substring(1,word.length()))) return true; //go left
-        if(wordSearchHelper(i, j+1, board.length, board[0].length, board, word.substring(1,word.length()))) return true; // go right
-        if(wordSearchHelper(i-1, j, board.length, board[0].length, board, word.substring(1,word.length()))) return true; // go up
-        if(wordSearchHelper(i+1, j, board.length, board[0].length, board, word.substring(1,word.length()))) return true; // go down
+        if(wordSearchHelper(i, j-1, row, col, board, word, index+1)) return true; //go left
+        if(wordSearchHelper(i, j+1, row, col, board, word, index+1)) return true; // go right
+        if(wordSearchHelper(i-1, j, row, col, board, word, index+1)) return true; // go up
+        if(wordSearchHelper(i+1, j, row, col, board, word, index+1)) return true; // go down
         board[i][j] = temp;
         return false;
     }
