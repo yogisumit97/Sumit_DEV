9e09be7f1f2144d42e8c107d816bda3be7189e8e
Sumit yogi <yogisumit97@gmail.com>
2025-04-16 10:21:52 +0530

    Time: 90 ms (90.56%), Space: 49.1 MB (84.41%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,13 +3,15 @@ class Solution {
         boolean[] arr = new boolean[n]; //we will check index 2 to index n-1 : initially all false.
         //let's consider false as prime and we will update true when non prime.
         int count = 0;
-        for(int i =2 ;i<arr.length; i++){
+        for(int i =2 ;i*i<arr.length; i++){ // till sqrt only
             if(arr[i]==true) continue; //non prime marked already
-            count++; //increase prime count;
             //3 6 9 12 .... mark 3 as prime and 3*3 onwards non prime check >> 3*2 already checked by 2
             for(long j = (long)i*i; j<arr.length; j=j+i){
                 arr[(int)j] = true; // marked non prime
             }
+        }
+        for(int i =2; i<arr.length;i++){
+            if(arr[i]==false) count++;
         }
         return count;
     }
