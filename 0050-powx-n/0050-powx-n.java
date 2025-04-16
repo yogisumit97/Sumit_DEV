851d2e9c057df578f45578de7c0ee5720e7f09ff
Sumit yogi <yogisumit97@gmail.com>
2025-04-16 10:49:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,13 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
+        if(n==1) return x;
         if(n==0 || x==1) return 1;
-        if(x==-1) return n%2==0 ? 1 : -1;
-        if(n== Integer.MIN_VALUE) return 0;
-        if(n<0) return myPow(1/x, Math.abs(n));
-        
-        double half = myPow(x,n/2);
-        double result = half*half;
-        if(n%2==1) result*=x;
-        return result;  
+        if(x==-1){
+            int temp=1;
+            if(n%2!=0) temp=-1;
+            return temp;
+        }
+        if(n==Integer.MIN_VALUE) return 0;
+        int sign = 1;// 1 for positive and -1 for negative.
+        if(n<0){
+            sign = -1;
+            n*=sign; // make n positive
+        }
+        double result = 0;
+        if(n%2==0) result = myPow(x*x, n/2);
+        else result=  x*myPow(x*x,n/2);
+        if(sign<0) return 1/result;
+        else return result; 
     }
 }
