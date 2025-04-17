53a27dc257c7254b1f4c94a3def76a307c18cae8
Sumit yogi <yogisumit97@gmail.com>
2025-04-17 16:01:00 +0530

    Time: 8 ms (5.29%), Space: 43.6 MB (66.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] max = new int[nums2.length];
+        Stack<Integer> stack = new Stack<>(); //keeps track of next max
         int count = nums2.length-1;
         while(count>=0){
-            int res = -1;
-            for(int i = count+1; i<nums2.length;i++){
-                if(nums2[i]>nums2[count]){
-                    res = nums2[i];
-                    break;
+            if(stack.empty()){
+                stack.push(nums2[count]);
+                max[count] = -1;
+            }
+            else{
+                if(stack.peek()>nums2[count]){
+                    max[count]= stack.peek();
+                    stack.push(nums2[count]);
+                }
+                else{
+                    while(!stack.empty() && stack.peek()<nums2[count]){
+                        stack.pop();
+                    }
+                    if(stack.empty()){
+                        max[count]=-1;
+                    }
+                    else{
+                        max[count]= stack.peek();
+                    }
+                    stack.push(nums2[count]);
                 }
             }
-            max[count] = res; 
             count--;
         }
         int[] ans = new int[nums1.length];
