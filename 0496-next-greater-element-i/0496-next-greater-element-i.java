e0b45c9b4207f51661912c907dcaf718b013a0a5
Sumit yogi <yogisumit97@gmail.com>
2025-04-17 16:11:50 +0530

    Time: 3 ms (89.32%), Space: 44.5 MB (24.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,40 +1,34 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] max = new int[nums2.length];
         Stack<Integer> stack = new Stack<>(); //keeps track of next max
         int count = nums2.length-1;
+        HashMap<Integer,Integer> hm = new HashMap<>();
         while(count>=0){
+            int temp = -1;
             if(stack.empty()){
                 stack.push(nums2[count]);
-                max[count] = -1;
+                temp = -1;
             }
             else{
                 if(stack.peek()>nums2[count]){
-                    max[count]= stack.peek();
+                    temp= stack.peek();
                     stack.push(nums2[count]);
                 }
                 else{
                     while(!stack.empty() && stack.peek()<nums2[count]){
                         stack.pop();
                     }
-                    if(stack.empty()){
-                        max[count]=-1;
-                    }
-                    else{
-                        max[count]= stack.peek();
-                    }
+                    if(stack.empty()) temp=-1;
+                    else temp= stack.peek();
                     stack.push(nums2[count]);
                 }
             }
+            hm.put(nums2[count],temp);
             count--;
         }
         int[] ans = new int[nums1.length];
-        for(int i =0; i<nums2.length; i++){
-            for(int j=0; j<nums1.length; j++){
-                if(nums1[j]==nums2[i]){
-                    ans[j]=max[i];
-                }
-            }
+        for(int j=0; j<nums1.length; j++){
+            ans[j] = hm.get(nums1[j]);
         }
         return ans;
     }
