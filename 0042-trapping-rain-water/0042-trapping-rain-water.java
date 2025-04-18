d48596d9f57d2b3bc2183f9cff4c49dc969e87f0
Sumit yogi <yogisumit97@gmail.com>
2025-04-18 12:24:49 +0530

    Time: 1 ms (63.53%), Space: 46.2 MB (73.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,26 @@
 class Solution {
     public int trap(int[] height) {
         int len = height.length;
-        int[] nextGreatest = new int[height.length];
-        int[] prevGreatest = new int[height.length];
+        int[] nextGreatest = new int[height.length]; // keep track of next greatest element
         int temp = -1;
         for(int i = len-1; i>=0; i--){
             nextGreatest[i] = Math.max(height[i], temp);
             temp = nextGreatest[i];
         }
-        temp = -1;
-        for(int i = 0; i<len; i++){
+        temp = -1;   // to store prevGreatest occured till now
+        int vol = 0;
+        for(int i =0; i<len; i++){
+            temp = Math.max(height[i], temp);
+            vol+= Math.min(temp, nextGreatest[i]) - height[i];
+        }
+        return vol;
+    }
+}
+
+/*
+temp = -1;
+for(int i = 0; i<len; i++){
             prevGreatest[i] = Math.max(height[i], temp);
             temp = prevGreatest[i];
         }
-        temp = 0;  // now temp will store volume
-        for(int i =0; i<len; i++){
-            temp+= Math.min(prevGreatest[i], nextGreatest[i]) - height[i];
-        }
-        return temp;
-    }
-}
+        */
