fe3fb3cc3d82bc776a0aff95be912db4a4c0a3c6
Sumit yogi <yogisumit97@gmail.com>
2025-04-19 13:26:16 +0530

    Time: 64 ms (59.43%), Space: 53.6 MB (6.17%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -5,19 +5,15 @@ class Solution {
         int[] nseIndex = new int[arr.length];// stores next smaller index, not equal to avoid double counting of similar elements
         Stack<Integer> stack = new Stack<>();  //to get PSE and NSE
         for(int i =0; i<len ; i++){
-            int res = -1;
             while(!stack.empty() && arr[stack.peek()]>=arr[i]) stack.pop();
-            if(!stack.empty()) res = stack.peek();
-            stack.push(i);
-            pseIndex[i] = res;
+            pseIndex[i] = stack.empty() ? -1 : stack.peek();
+            stack.push(i); 
         }
         stack.clear();
         for(int i =len-1; i>=0 ; i--){
-            int res = len;
             while(!stack.empty() && arr[stack.peek()]>arr[i]) stack.pop(); // only greater check and not equal as in pse to avoid double taking similar elements for ex check with array :   {1,1} >> if it is not done answer comes 4 whereas it should be 3
-            if(!stack.empty()) res = stack.peek();
+            nseIndex[i] = stack.empty() ? len : stack.peek();
             stack.push(i);
-            nseIndex[i] = res;
         }
         long sum = 0;
         for(int i =0; i<len ; i++){
