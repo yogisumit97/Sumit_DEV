c2bde489603aa8bb4d632e148a149c18f2f775d6
Sumit yogi <yogisumit97@gmail.com>
2025-04-19 16:11:06 +0530

    Time: 7 ms (45.88%), Space: 45.5 MB (34.56%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -7,34 +7,22 @@ class Solution {
             if(firstPositive==Integer.MAX_VALUE && asteroids[i]>0) firstPositive = i;
             if(asteroids[i]<0) lastNegative = i;
         }
-        //System.out.println(firstPositive+ " "+ lastNegative);
         if(firstPositive >  lastNegative) return asteroids;
         for(int i = firstPositive; i<= lastNegative; i++){
             if(stack.empty() || stack.peek()*asteroids[i]>0 || stack.peek()<0){
                 stack.push(asteroids[i]);
-                //System.out.println("Stack is : "+ stack); 
                 continue;
             }
             while(!stack.empty() && stack.peek()*asteroids[i]<0 && Math.abs(stack.peek()) < Math.abs(asteroids[i])) stack.pop();
             if(stack.empty() || stack.peek()*asteroids[i]>0)  stack.push(asteroids[i]);
             else if(Math.abs(stack.peek()) == Math.abs(asteroids[i])) stack.pop();
-            //System.out.println("Stack is : "+ stack); 
         }
         int[] result = new int[stack.size()+ firstPositive + asteroids.length - 1- lastNegative];
         int count = 0;
         for(int i =0; i<firstPositive; i++) result[count++] = asteroids[i];
-        /*System.out.print("1 >> ");
-        for(int i : result) System.out.print(i+" ");
-        System.out.println();*/
         for(int i = stack.size()-1; i>=0; i--) result[count+i] = stack.pop();
         count = result.length-1;
-        /*System.out.print("2 >> ");
-        for(int i : result) System.out.print(i+" ");
-        System.out.println();*/
         for(int i =asteroids.length-1 ; i>lastNegative ; i--) result[count--] = asteroids[i];
-        /*System.out.print("3 >> ");
-        for(int i : result) System.out.print(i+" ");
-        System.out.println();*/
         return result;
     }
 }
