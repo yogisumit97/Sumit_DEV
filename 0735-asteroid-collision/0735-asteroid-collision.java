e36553d5c2ab91aaa802ce3edd4a8f3bc919db35
Sumit yogi <yogisumit97@gmail.com>
2025-04-19 16:47:05 +0530

    Time: 9 ms (10.15%), Space: 45.5 MB (54.1%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,14 +1,7 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        int firstPositive = Integer.MAX_VALUE;
-        int lastNegative = Integer.MIN_VALUE;
-        for(int i=0; i<asteroids.length; i++){
-            if(firstPositive==Integer.MAX_VALUE && asteroids[i]>0) firstPositive = i;
-            if(asteroids[i]<0) lastNegative = i;
-        }
-        if(firstPositive >  lastNegative) return asteroids;
-        for(int i = firstPositive; i<= lastNegative; i++){
+        for(int i = 0; i< asteroids.length; i++){
             if(stack.empty() || stack.peek()*asteroids[i]>0 || stack.peek()<0){
                 stack.push(asteroids[i]);
                 continue;
@@ -17,12 +10,8 @@ class Solution {
             if(stack.empty() || stack.peek()*asteroids[i]>0)  stack.push(asteroids[i]);
             else if(Math.abs(stack.peek()) == Math.abs(asteroids[i])) stack.pop();
         }
-        int[] result = new int[stack.size()+ firstPositive + asteroids.length - 1- lastNegative];
-        int count = 0;
-        for(int i =0; i<firstPositive; i++) result[count++] = asteroids[i];
-        for(int i = stack.size()-1; i>=0; i--) result[count+i] = stack.pop();
-        count = result.length-1;
-        for(int i =asteroids.length-1 ; i>lastNegative ; i--) result[count--] = asteroids[i];
+        int[] result = new int[stack.size()];
+        for(int i =stack.size()-1; i>=0; i--) result[i] = stack.pop();
         return result;
     }
 }
