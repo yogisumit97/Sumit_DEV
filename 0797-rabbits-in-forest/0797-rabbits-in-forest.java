9410173ac18aad9268b590b086b1f7cfdec8207a
Sumit yogi <yogisumit97@gmail.com>
2025-04-20 11:27:54 +0530

    Time: 4 ms (30.84%), Space: 42.2 MB (81.02%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -2,9 +2,11 @@ class Solution {
     public int numRabbits(int[] answers) {
         HashMap<Integer,Integer> mpp = new HashMap<>();
         int ans = 0;
-        for(int num : answers) mpp.put(num+1, mpp.getOrDefault(num+1,0)+1);
+        for(int num : answers) mpp.put(num, mpp.getOrDefault(num,0)+1);
         for(Map.Entry<Integer, Integer> mapEntry : mpp.entrySet()){
-            ans+= mapEntry.getKey()*(Math.ceil(mapEntry.getValue()*1.0/mapEntry.getKey()));
+            int num = mapEntry.getKey()+1;
+            int res = mapEntry.getValue();
+            ans+= num*(Math.ceil(res*1.0/num));
         }
         return ans;
     }
