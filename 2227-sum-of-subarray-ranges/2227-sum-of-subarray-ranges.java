b04a2ed205eb92c298ca7ff1c9455c2d9eb83fb8
Sumit yogi <yogisumit97@gmail.com>
2025-04-20 14:36:00 +0530

    Time: 41 ms (22.71%), Space: 44.4 MB (68.25%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,15 +1,55 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        int len = nums.length;
+        int[] pseeIndex = findPseeHelper(nums, len);
+        int[] nseIndex = findNseHelper(nums, len);
+        int[] pgeeIndex = findPgeeHelper(nums,len);
+        int[] ngeIndex = findNgeHelper(nums, len);
         long sum = 0;
-        for(int i =0; i<nums.length-1; i++){
-            int min = nums[i];
-            int max = nums[i];    
-            for(int j=i+1; j<nums.length; j++){
-                min = Math.min(min,nums[j]);
-                max = Math.max(max,nums[j]);
-                sum+= max-min;
-            }
+        for(int i =0; i<len ; i++){
+            sum+= (1L*nums[i]*(i-pgeeIndex[i])*(ngeIndex[i]-i));
+            sum-= (1L*nums[i]*(i-pseeIndex[i])*(nseIndex[i]-i));
         }
         return sum;
+    }
+    private int[] findPseeHelper(int[] arr, int len){
+        int[] pseIndex = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();  
+        for(int i =0; i<len ; i++){
+            while(!stack.empty() && arr[stack.peek()]>=arr[i]) stack.pop();
+            pseIndex[i] = stack.empty() ? -1 : stack.peek();
+            stack.push(i); 
+        }
+        return pseIndex;
+    }
+    private int[] findNseHelper(int[] arr, int len){
+        int[] nseIndex = new int[arr.length];
+        Stack<Integer> stack = new Stack<>(); 
+        for(int i =len-1; i>=0 ; i--){
+            while(!stack.empty() && arr[stack.peek()]>arr[i]) stack.pop(); 
+            nseIndex[i] = stack.empty() ? len : stack.peek();
+            stack.push(i);
+        }
+        return nseIndex;
+    }
+    private int[] findPgeeHelper(int[] arr, int len){
+        int[] pgeIndex = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();  
+        for(int i =0; i<len ; i++){
+            while(!stack.empty() && arr[stack.peek()]<=arr[i]) stack.pop();
+            pgeIndex[i] = stack.empty() ? -1 : stack.peek();
+            stack.push(i); 
+        }
+        return pgeIndex;
+    }
+    private int[] findNgeHelper(int[] arr, int len){
+        int[] ngeIndex = new int[arr.length];
+        Stack<Integer> stack = new Stack<>(); 
+        for(int i =len-1; i>=0 ; i--){
+            while(!stack.empty() && arr[stack.peek()]<arr[i]) stack.pop(); 
+            ngeIndex[i] = stack.empty() ? len : stack.peek();
+            stack.push(i);
+        }
+        return ngeIndex;
     }
 }
