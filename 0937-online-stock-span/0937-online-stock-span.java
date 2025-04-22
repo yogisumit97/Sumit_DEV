882e4a31187b931d35582ef00d5d73c0b1c1fc5f
Sumit yogi <yogisumit97@gmail.com>
2025-04-22 12:26:39 +0530

    Time: 34 ms (20.83%), Space: 55.6 MB (40.35%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,15 +1,16 @@
 class StockSpanner {
     Stack<List<Integer>> stack;//monotonic stack; store in decreaing order price + no. of day on which it happened
+    int count;
     public StockSpanner() {
-        stack = new Stack<>();
+        stack = new Stack<>(); // stores value and index starting 0;
+        count = 0;
     }
     public int next(int price) {
-        int count =stack.empty()? 0 : stack.peek().get(1)+1;
         while(!stack.empty() && stack.peek().get(0)<= price) stack.pop();
         int temp = stack.empty()? count+1 : count - stack.peek().get(1);
         List<Integer> list = new ArrayList<>();
         list.add(price);
-        list.add(count);
+        list.add(count++);
         stack.push(list);
         return temp;        
     }
