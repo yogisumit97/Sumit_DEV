ff4882aa32d1222e007ec5cdc51a404c2737e9ab
Sumit yogi <yogisumit97@gmail.com>
2025-04-22 10:35:13 +0530

    Time: 16 ms (55.98%), Space: 46.6 MB (54.38%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,31 +1,39 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         int maxArea = Integer.MIN_VALUE;
-        for(int i=1; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){ // store prefix sum
-                if(matrix[i][j]=='0') continue;
-                matrix[i][j] = (char)(matrix[i-1][j]+1);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] mat = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){ // store prefix sum
+                if(i==0){
+                    mat[i][j] = (int)(matrix[i][j]-'0');
+                }
+                else if(matrix[i][j]=='0'){
+                    mat[i][j] = 0;
+                }
+                else mat[i][j] = mat[i-1][j]+1;
             }
         }
-        for(int i =0; i<matrix.length; i++){
-            maxArea = Math.max(maxArea,largestRectangleArea(matrix[i]));
+        for(int i =0; i<m; i++){
+            maxArea = Math.max(maxArea,largestRectangleArea(mat[i]));
         }
         return maxArea;
     }
-    public int largestRectangleArea(char[] heights) {
+    public int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>(); //keeps indexes in Increasing order of heights
         int len = heights.length;
         int maxArea = Integer.MIN_VALUE;
         for(int i =0; i<len; i++){
             while(!stack.empty() && heights[stack.peek()]>heights[i]){
-                int currNum = heights[stack.pop()]-'0';
+                int currNum = heights[stack.pop()];
                 int pseIndex = stack.empty() ? -1 :stack.peek();//after pop,top ele becomes pse;
                 maxArea = Math.max(maxArea, currNum*(i-pseIndex-1));// we already know nse is i
             }
             stack.push(i);
         }
         while(!stack.empty()){ // remaining stack had no nse, thus nse = len for calculation
-            int currNum = heights[stack.pop()]-'0';
+            int currNum = heights[stack.pop()];
             int pseIndex = stack.empty() ? -1 :stack.peek();
             maxArea = Math.max(maxArea, currNum*(len-pseIndex-1));
         }
