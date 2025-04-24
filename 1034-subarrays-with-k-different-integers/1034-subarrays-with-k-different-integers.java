1796fc6d0ee62103c0e753fb6aaabdb917e7dea3
Sumit yogi <yogisumit97@gmail.com>
2025-04-24 12:59:41 +0530

    Time: 51 ms (25.74%), Space: 47.8 MB (33.88%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -3,8 +3,6 @@ class Solution {
         return  kDistinctSubarratHelper(k, nums)- kDistinctSubarratHelper(k-1, nums);// at most k - at most(k-1);
     }
     private static int kDistinctSubarratHelper(int k,int[] nums) {
-        //System.out.println();
-        //System.out.println("For K =="+k+" :" );
         if(k<=0) return 0;
 		HashMap<Integer,Integer> hm = new HashMap<>();//freq store
 		int l = 0;
@@ -13,20 +11,14 @@ class Solution {
 		while(r<nums.length){
             int curr = nums[r];
 			hm.put(curr, hm.getOrDefault(curr,0)+1);
-            //System.out.println(hm+"    Size : "+ hm.size());
-			if(hm.size()>k){ // shrink window
+            while(hm.size()>k){ // shrink window
 				int temp =nums[l];
 				hm.put(temp, hm.get(temp)-1);
 				if(hm.get(temp)==0) hm.remove(temp);
-                //System.out.println("after deleting "+temp+" :"+hm);
-				l = l+1;
-                //System.out.println("left = "+l);
-                hm.put(curr, hm.get(curr)-1);
-                continue;
+                l = l+1;
 			}
-            if(hm.size()<=k) maxLen+= r-l+1;
-            //System.out.println(r-l+1);
-			r++;
+            maxLen+= r-l+1;
+            r++;
 		}
 		return maxLen;
 	}
