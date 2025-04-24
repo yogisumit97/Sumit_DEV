5acc98686a6f891229df8b8ea3ff87869984ce68
Sumit yogi <yogisumit97@gmail.com>
2025-04-24 09:41:33 +0530

    Time: 12 ms (57.18%), Space: 55.5 MB (24.15%) - LeetHub

---
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return findSubArrayHelper(nums,k)-findSubArrayHelper(nums,k-1);
+    }
+    private int findSubArrayHelper(int[] nums,int goal){ // find subarrays with less or equal odd than goal
+        if(goal<0) return 0;
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        int odd = 0;
+        while(r<nums.length){
+            if((nums[r]&1) !=0) odd = odd+1;
+            while(odd>goal){
+                if((nums[l]&1) !=0) odd = odd-1;
+                l=l+1;
+            }
+            count+=r-l+1; //increase count by len >> subarraysEnding here.
+            r++;
+        }
+        System.out.println(count);
+        return count;
+    }
+}
