6cd08a1e8729b54c5f944390a0b73299d15720f0
Sumit yogi <yogisumit97@gmail.com>
2025-04-24 18:02:16 +0530

    Time: 99 ms (19.58%), Space: 45.7 MB (15.47%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,26 +8,19 @@ class Solution {
         int count = 0;
         while(r<s.length()){
             char curr = s.charAt(r);
-            if(hash[curr]<=0){
-                hash[curr]--;    
-            }
-            else{ //exists in t and s both
-                hash[curr]--;
-                count++;
+            if(hash[curr]>0){//exists in t and s both
+                count++;    
             }
             while(count==t.length()){ // shrink window
-                //System.out.println(s.substring(l,r+1));
-                if(answer.equals("")) answer = s.substring(l,r+1);
+                if(answer.length()==0) answer = s.substring(l,r+1);
                 else answer = answer.length() > r-l+1 ? s.substring(l,r+1) : answer;
                 char temp = s.charAt(l);
                 hash[temp]++;
                 l++;
                 if(hash[temp]>0) count--;
-                //System.out.println("Equal now : "+answer+" "+" "+l+" "+count);
                 continue;
             }
-            //System.out.print(count+" ");
-            //System.out.println(curr);
+            hash[curr]--;
             r++;
         }
         return answer;
