0c8402c70cac98774e11ffa17b095a4e0eef6a7d
Sumit yogi <yogisumit97@gmail.com>
2025-04-26 17:33:53 +0530

    Time: 4 ms (16.36%), Space: 45.9 MB (42.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int len = nums.length;
         int nextPoint = 0;
         int i =0;
-        while(i<nums.length){
+        while(i<len){
             int maxDistancePossible = nums[i];
-            if(i+maxDistancePossible+1 >= nums.length) return true;
-            for(int  j = i+1; j<nums.length && j<=i+nums[i]; j++){
+            if(maxDistancePossible+i+1 >= len) return true;
+            for(int  j = i+1; j<len && j<=i+nums[i]; j++){
                 int possibleDistance = j-i+nums[j];
                 if(maxDistancePossible <= possibleDistance){
                     maxDistancePossible = possibleDistance;
                     nextPoint = j;
                 }
             }
-            if(nextPoint!=nums.length-1 && nums[nextPoint]==0) return false;
+            if(nextPoint!=len-1 && nums[nextPoint]==0) return false;
             i = nextPoint;
         }
         return true;
