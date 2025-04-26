83063de3aa52f8bd07d3c3858d7612be8e126da2
Sumit yogi <yogisumit97@gmail.com>
2025-04-26 17:43:19 +0530

    Time: 2 ms (81.43%), Space: 45.7 MB (70.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,9 +3,9 @@ class Solution {
         int len = nums.length;
         int maxDistanceIndex = 0;
         for(int i =0; i<len; i++){
+            if(i>maxDistanceIndex) return false;
             maxDistanceIndex = Math.max(maxDistanceIndex, i+nums[i]);
-            if(maxDistanceIndex == i && i!=len-1) return false;
-            else if(maxDistanceIndex>=len-1) return true;
+            if(maxDistanceIndex>=len-1) return true;
         }
         return true;
     }
