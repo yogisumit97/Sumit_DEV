9c9028b80c9b33592ed8632b34229f8fb3be6498
Sumit yogi <yogisumit97@gmail.com>
2025-04-27 11:37:44 +0530

    Time: 3 ms (84.59%), Space: 45.5 MB (90.8%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,44 +1,20 @@
 class Solution {
-    private class Rating {
-        int rating;
-        int index;
-        Rating(int rating , int index){
-            this.rating = rating;
-            this.index = index;
-        }
-    }
     public int candy(int[] ratings) {
         int len = ratings.length;
-        if(len==1) return 1;
-        Rating[] rating = new Rating[len];
-        for(int i =0; i<len; i++){
-            rating[i] = new Rating(ratings[i], i);
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0]=1;
+        for(int i =1; i<len; i++){
+            if(ratings[i]>ratings[i-1]) left[i] = left[i-1]+1;
+            else left[i]=1;
         }
-        Arrays.sort(rating, new Comparator<Rating>(){
-            @Override
-            public int compare(Rating a, Rating b){
-                return Integer.compare(a.rating, b.rating);
-            }
-        });
-        int[] candies = new int[len]; // initially 0 candies;
+        right[len-1] = 1;
+        for(int i =len-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]) right[i] = right[i+1]+1;
+            else right[i]=1;
+        }
         int maxCandies = 0;
-        for(Rating r : rating){ //r.rating is present rating which is in ascending order
-            int i = r.index;
-            if(i!=0 && i!=len-1){
-                if(ratings[i]==ratings[i-1]){
-                    if(ratings[i]<=ratings[i+1]) candies[i] = 1;
-                    else candies[i] = candies[i+1]+1;
-                }
-                else candies[i] = Math.max(candies[i-1], candies[i+1])+1;
-            }
-            else if(i==0) candies[i] = candies[i+1]+1;
-            else{
-                if(ratings[i]==ratings[i-1]) candies[i]=1;
-                else candies[i] = candies[i-1]+1;
-            }
-            maxCandies+=candies[i];
-        }
+        for(int i =0; i<len; i++) maxCandies+= Math.max(left[i], right[i]);
         return maxCandies;
     }
 }
-
