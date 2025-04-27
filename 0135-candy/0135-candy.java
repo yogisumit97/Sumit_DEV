842cc944d38e6de0a7caf60f2b61d356eb99f82b
Sumit yogi <yogisumit97@gmail.com>
2025-04-27 11:40:02 +0530

    Time: 4 ms (26.89%), Space: 45.6 MB (86.73%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,18 +3,18 @@ class Solution {
         int len = ratings.length;
         int[] left = new int[len];
         int[] right = new int[len];
-        left[0]=1;
-        for(int i =1; i<len; i++){
-            if(ratings[i]>ratings[i-1]) left[i] = left[i-1]+1;
-            else left[i]=1;
-        }
         right[len-1] = 1;
         for(int i =len-2; i>=0; i--){
             if(ratings[i]>ratings[i+1]) right[i] = right[i+1]+1;
             else right[i]=1;
         }
-        int maxCandies = 0;
-        for(int i =0; i<len; i++) maxCandies+= Math.max(left[i], right[i]);
-        return maxCandies;
+        int maxCandies = 0;   
+        left[0]=1;
+        for(int i =1; i<len; i++){
+            if(ratings[i]>ratings[i-1]) left[i] = left[i-1]+1;
+            else left[i]=1;
+            maxCandies+= Math.max(left[i], right[i]);
+        }
+        return maxCandies+Math.max(left[0],right[0]);
     }
 }
