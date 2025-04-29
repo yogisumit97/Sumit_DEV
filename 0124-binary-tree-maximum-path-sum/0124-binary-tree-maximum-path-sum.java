95fb49acb00998865f1220ee9d7f6c34bf61dce9
Sumit yogi <yogisumit97@gmail.com>
2025-04-29 14:34:41 +0530

    Time: 0 ms (100%), Space: 44.2 MB (89.24%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -23,26 +23,28 @@ class Solution {
     }
     public int maxSumFinder(TreeNode root){
         if(root== null) return 0;
+        int lh = Integer.MIN_VALUE;
+        int rh = Integer.MIN_VALUE;
+        int max = 0;
         if(root.left==null && root.right==null){
             maxSum = Math.max(maxSum, root.val);
             return root.val;
         }
-        int max = 0;
-        if(root.left!=null && root.right!=null){
-            int lh = maxSumFinder(root.left);
-            int rh = maxSumFinder(root.right);
+        else if(root.left!=null && root.right!=null){
+            lh = maxSumFinder(root.left);
+            rh = maxSumFinder(root.right);
             max = Math.max(Math.max(lh+rh+root.val, root.val), Math.max(lh+root.val, rh+root.val));
             maxSum = Math.max(maxSum, max );
             return Math.max(root.val, Math.max(lh+root.val, rh+root.val));
         }
         else if(root.left == null){
-            int rh = maxSumFinder(root.right);
+            rh = maxSumFinder(root.right);
             max = Math.max(Math.max(rh+root.val, root.val), rh);
             maxSum = Math.max(maxSum, max );
             return Math.max(root.val, rh+root.val);
         }
         else{
-            int lh = maxSumFinder(root.left);
+            lh = maxSumFinder(root.left);
             max = Math.max(Math.max(lh+root.val, root.val), lh);
             maxSum = Math.max(maxSum, max );
             return Math.max(root.val, lh+root.val);
