a292d1640f80899739b58b35bb9edadeef38372b
Sumit yogi <yogisumit97@gmail.com>
2025-05-01 04:17:59 +0530

    Time: 2 ms (31%), Space: 45.2 MB (61.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -6,8 +6,8 @@ class Solution {
         int i =0;
         while(i<len){
             int maxDistancePossible = nums[i];
-            if(i==len-1) return jumps;
-            if(maxDistancePossible+i+1 >= len) return jumps+1;
+            if(i==len-1) return jumps; //reached
+            else if(maxDistancePossible+i+1 >= len) return jumps+1; //next step reach
             for(int  j = i+1; j<len && j<=i+nums[i]; j++){
                 int possibleDistance = j-i+nums[j];
                 if(maxDistancePossible <= possibleDistance){
