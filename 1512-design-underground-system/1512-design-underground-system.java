0d2f9d6803503271b32461fd0ea5736e1ca5cbe5
Sumit yogi <yogisumit97@gmail.com>
2025-05-01 03:28:39 +0530

    Time: 95 ms (62.92%), Space: 55.1 MB (87.92%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.java
+++ 1512-design-underground-system/1512-design-underground-system.java
@@ -1,49 +1,25 @@
 class UndergroundSystem {
-    private class Customer{
-        int id;
-        String startStation;
-        String endStation;
-        int startTime;
-        int endTime;
-        Customer(int id, String stationName, int t){
-            this.id = id;
-            this.startStation = stationName;
-            this.endStation = "";
-            this.startTime = t;
-            this.endTime = -1;
-        }
-    }
-    List<Customer> customerList;
+    HashMap<Integer, Pair<String, Integer>> checkInMap; // id, <start station, startTime>
+    HashMap<String, Pair<Double, Integer>> routeMap; // root, <totaltime, total customers>
     public UndergroundSystem() {
-        customerList = new ArrayList<>();
+        checkInMap = new HashMap<>();
+        routeMap = new HashMap<>();    
     }
     public void checkIn(int id, String stationName, int t) {
-        Customer curr = new Customer(id, stationName, t);
-        customerList.add(curr);
+        checkInMap.put(id, new Pair<>(stationName, t));    
     }
     public void checkOut(int id, String stationName, int t) {
-        int index = customerList.size()-1;
-        while(index>=0){
-            Customer curr = customerList.get(index);
-            if(curr.id == id){
-                curr.endStation = stationName;
-                curr.endTime = t;
-                break;
-            }
-            index--;
-        }
+        Pair<String, Integer> currPair = checkInMap.get(id);
+        checkInMap.remove(id);
+        String routeName = currPair.getKey()+"_"+stationName; // concatenate using delimiter
+        int time = t - currPair.getValue();
+        Pair<Double, Integer> curr = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
+        routeMap.put(routeName, new Pair<>(curr.getKey()+time, curr.getValue()+1));
     }
     public double getAverageTime(String startStation, String endStation) {
-        int count = 0;
-        double sum = 0;
-        for(Customer customer : customerList){
-            if(customer.startStation.equals(startStation) && customer.endStation.equals(endStation)){
-                sum+= customer.endTime - customer.startTime;
-                count++;
-            }
-        }
-        if(count ==0) return 0;
-        return sum/count;
+        String route = startStation+"_"+endStation;
+        Pair<Double, Integer> curr =  routeMap.get(route);
+        return curr.getKey()/curr.getValue();
     }
 }
 
