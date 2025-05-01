710b9c2873fd081fe0ea04664172263342d962f1
Sumit yogi <yogisumit97@gmail.com>
2025-05-01 18:37:45 +0530

    Time: 4 ms (6.72%), Space: 47.2 MB (94.65%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -3,18 +3,22 @@ class Solution {
         if(root==null) return 0;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        int level = 2;
+        int total = 0;
         while(q.size()!=0){
             int size = q.size();
             for(int i =0; i<size; i++){
                 TreeNode curr = q.poll();
-                if(curr.left!=null) q.offer(curr.left);
-                else return level+q.size()-(size-i);
-                if(curr.right!=null) q.offer(curr.right);
-                else return level+q.size()-(size-i);
+                total+=1;
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                else return total+q.size();
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
+                else return total+q.size();
             }
-            level = level<<1;
         }
-        return 0;       
+        return total;       
     }
 }
