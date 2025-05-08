ab7e87d48b26b58affc794d67fcb3b35f7612394
Sumit yogi <yogisumit97@gmail.com>
2025-05-08 16:56:18 +0530

    Time: 2 ms (68.63%), Space: 42.5 MB (62.33%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,6 +1,5 @@
 class Solution {
     public int orangesRotting(int[][] grid) { // using BFS
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         Queue<Pair<Integer,Integer>> q = new LinkedList<>();
         boolean rottenExists = false;
         boolean freshExists = false;
@@ -8,7 +7,6 @@ class Solution {
             for(int j=0; j<grid[0].length; j++){ // put all rotten in queue initially.
                 if(grid[i][j]==2){
                     q.offer(new Pair(i,j));
-                    visited[i][j] = true;
                     rottenExists = true;
                 }
                 else if(grid[i][j]==1) freshExists = true;
@@ -25,22 +23,18 @@ class Solution {
                 int j = pair.getValue();
                 if(j-1 >=0 && grid[i][j-1]==1){// left case
                     grid[i][j-1] = 2; // mark rotten
-                    visited[i][j-1] = true;
                     q.offer(new Pair(i,j-1));
                 }
                 if(j+1 < grid[0].length && grid[i][j+1]==1){//right case
                     grid[i][j+1] = 2; // mark rotten
-                    visited[i][j+1] = true;
                     q.offer(new Pair(i,j+1));
                 }
                 if(i-1 >=0 && grid[i-1][j]==1){// up case
                     grid[i-1][j] = 2; // mark rotten
-                    visited[i-1][j] = true;
                     q.offer(new Pair(i-1,j));
                 }
                 if(i+1 < grid.length && grid[i+1][j]==1){// down case
                     grid[i+1][j] = 2; // mark rotten
-                    visited[i+1][j] = true;
                     q.offer(new Pair(i+1,j));
                 }
             }
