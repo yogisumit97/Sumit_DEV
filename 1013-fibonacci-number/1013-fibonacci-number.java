1264e442521819292b24bac8db64ec077c2046c8
Sumit yogi <yogisumit97@gmail.com>
2025-05-14 17:43:51 +0530

    Time: 8 ms (43.4%), Space: 40.8 MB (18.57%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,11 +1,6 @@
-class Solution {
-    public int[] temp = new int[31];
+class Solution { // recursion method
     public int fib(int n) {
-        if(temp[n]!=0) return temp[n];
-        if(n==0 || n==1) return n;
-        else{
-            temp[n] = fib(n-1) + fib(n-2);
-            return temp[n];
-        }
+        if(n<=1) return n;
+        return fib(n-1)+ fib(n-2);
     }
 }
