42177caa0caf08251ce290bbcd67b675ec827731
Sumit yogi <yogisumit97@gmail.com>
2025-05-14 17:47:58 +0530

    Time: 0 ms (100%), Space: 40.4 MB (64.08%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,11 @@
-class Solution { // memoization method
+class Solution { // tabulation method > no recursion stack space in space complexity
     public int[] dp = new int[31];
     public int fib(int n) {
-        if(n<=1) return n;
-        if(dp[n] !=0) return dp[n];
-        return dp[n] = fib(n-1)+ fib(n-2);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
