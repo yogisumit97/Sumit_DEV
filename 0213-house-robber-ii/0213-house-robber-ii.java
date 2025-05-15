250187fca13dc525193601bb3f94c1019603b6d4
Sumit yogi <yogisumit97@gmail.com>
2025-05-15 12:04:44 +0530

    Time: 0 ms (100%), Space: 40.9 MB (90.36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp1 = new int[nums.length];
-        int[] dp2 = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            dp1[i] = dp2[i] = -1;
-        }
-        int excludingFirst = robHelper(1,nums.length-1, nums, dp1);
-        int excludingLast = robHelper(0,nums.length-2, nums, dp2);
-        return Math.max(excludingFirst, excludingLast);
-    }
-    private int robHelper(int i, int j, int[] nums, int[] dp){
+        if(nums.length==0) return 0;
         if(nums.length==1) return nums[0];
-        if(j==i) return nums[i];
-        if(j==i+1) return Math.max(nums[i+1], nums[i]);
-        if(dp[j]!=-1) return dp[j];
-        return dp[j] = Math.max(robHelper(i,j-1, nums, dp), nums[j] + robHelper(i,j-2, nums, dp));
+        if(nums.length==2) return Math.max(nums[0], nums[1]);
+        int[] dp1 = new int[nums.length-1];
+        int[] dp2 = new int[nums.length-1];
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(dp1[0], nums[1]);
+        dp2[0] = nums[1];
+        dp2[1] = Math.max(dp2[0], nums[2]);
+        for(int i=2; i<=nums.length-2; i++){
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2]+nums[i]);
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2]+nums[i+1]);
+        }
+        return Math.max(dp2[dp2.length-1], dp1[dp1.length-1]);
     }
 }
