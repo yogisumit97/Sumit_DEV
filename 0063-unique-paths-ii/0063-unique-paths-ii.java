f1db3eb7805ea155ed866c97b4fe45a2bae5cf7d
Sumit yogi <yogisumit97@gmail.com>
2025-05-22 14:46:39 +0530

    Time: 0 ms (100%), Space: 41.8 MB (52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,14 +4,21 @@ class Solution {
         int n = og.length;
         int[][] dp = new int[n][m];
         for(int i =0; i<n; i++){
-            for(int j=0; j<m; j++) dp[i][j] = -1;
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0){
+                    if(og[i][j]==1) return 0;
+                    dp[i][j] = 1;
+                }
+                else if(og[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    int left = j-1>=0 ? dp[i][j-1] : 0;
+                    int up = i-1>=0 ? dp[i-1][j] : 0;
+                    dp[i][j] = left + up;
+                }
+            }
         }
-        return helper(n-1, m-1, og, dp);
-    }
-    private int helper(int i, int j, int[][] og, int[][] dp){
-        if(i<0 || j<0 || og[i][j] ==1) return 0;
-        if(i ==0 && j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j] = helper(i-1, j, og, dp) + helper(i, j-1, og, dp);// up + left
-    }
+        return dp[n-1][m-1];
+    }    
 }
