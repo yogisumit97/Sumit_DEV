3467e280eea3199d704f96a22d05c2ee099a375b
Sumit yogi <yogisumit97@gmail.com>
2025-05-22 14:42:42 +0530

    Time: 0 ms (100%), Space: 41.6 MB (91.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m+1][n+1];
-        for(int i =0; i<m+1; i++){
-            for(int j=0; j<n+1; j++) dp[i][j] = -1;
+    public int uniquePathsWithObstacles(int[][] og) {
+        int m = og[0].length;
+        int n = og.length;
+        int[][] dp = new int[n][m];
+        for(int i =0; i<n; i++){
+            for(int j=0; j<m; j++) dp[i][j] = -1;
         }
-        return uniquePathHelper(0,0,m,n, dp, obstacleGrid);
+        return helper(n-1, m-1, og, dp);
     }
-    private int uniquePathHelper(int i, int j, int m, int n, int[][] dp, int[][] obstacleGrid){
-        if(i>=m || j>=n || obstacleGrid[i][j] ==1) return 0;
-        if(i== m-1 && j==n-1) return 1;
-        if(dp[i][j+1] == -1){
-            dp[i][j+1] = uniquePathHelper(i, j+1, m, n, dp, obstacleGrid); // move right 
-        }
-        if(dp[i+1][j] == -1){
-            dp[i+1][j] = uniquePathHelper(i+1, j, m, n, dp, obstacleGrid); // move down
-        }
-        return dp[i][j+1] + dp[i+1][j];
+    private int helper(int i, int j, int[][] og, int[][] dp){
+        if(i<0 || j<0 || og[i][j] ==1) return 0;
+        if(i ==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = helper(i-1, j, og, dp) + helper(i, j-1, og, dp);// up + left
     }
 }
