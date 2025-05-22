4b28359d9100242e8ea51821a68d601cfd39d05e
Sumit yogi <yogisumit97@gmail.com>
2025-05-22 16:56:12 +0530

    Time: 7 ms (58.51%), Space: 44.9 MB (94.07%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -5,20 +5,18 @@ class Solution {
         int minSum = Integer.MAX_VALUE;
         for(int i =0; i<len; i++){
             for(int j=0; j<len; j++){
-                dp[i][j] = Integer.MIN_VALUE;
+                if(i==0) dp[i][j] = matrix[i][j];
+                else{
+                    int dl = j-1>=0 ? dp[i-1][j-1] : Integer.MAX_VALUE;
+                    int du = dp[i-1][j];
+                    int dr = j+1 < len ? dp[i-1][j+1] : Integer.MAX_VALUE;
+                    dp[i][j] = matrix[i][j] + Math.min(dl, Math.min(du, dr));
+                }
             }
         }
         for(int i=0; i< len; i++){
-            minSum = Math.min(minSum, minSumHelper(0, i, matrix, dp));
+            minSum = Math.min(minSum, dp[len-1][i]);
         }
         return minSum;
-    }
-    private int minSumHelper(int i, int j, int[][] matrix, int[][] dp){
-        if(i==matrix.length-1) return matrix[i][j];
-        if(dp[i][j] !=Integer.MIN_VALUE) return dp[i][j];
-        int dl = j-1>=0 ? minSumHelper(i+1, j-1, matrix, dp) : Integer.MAX_VALUE;
-        int db = minSumHelper(i+1, j, matrix, dp);
-        int dr = j+1 < matrix.length ? minSumHelper(i+1, j+1, matrix, dp) : Integer.MAX_VALUE;
-        return dp[i][j] = matrix[i][j] + Math.min(dl, Math.min(db, dr));
     }
 }
