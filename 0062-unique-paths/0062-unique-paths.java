e73dea86811d2f57d2be41969578d5ac9ad84474
Sumit yogi <yogisumit97@gmail.com>
2025-05-22 13:33:13 +0530

    Time: 1 ms (8.41%), Space: 40.7 MB (37.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        for(int i =0; i<m+1; i++){
-            for(int j=0; j<n+1; j++) dp[i][j] = -1;
+        int[][] dp = new int[m][n];
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(i==m-1 && j==n-1){
+                    dp[m-1][n-1] = 1;
+                    continue;
+                }
+                int right = j+1< n ? dp[i][j+1] : 0; // right
+                int down = i+1 < m ? dp[i+1][j] : 0; // down
+                dp[i][j] = right + down;
+            }
         }
-        return uniquePathHelper(0,0,m,n, dp);
-    }
-    private int uniquePathHelper(int i, int j, int m, int n, int[][] dp){
-        if(i>=m || j>=n) return 0;
-        if(i== m-1 && j==n-1) return 1;
-        if(dp[i][j+1] == -1){
-            dp[i][j+1] = uniquePathHelper(i, j+1, m, n, dp); // move right 
-        }
-        if(dp[i+1][j] == -1){
-            dp[i+1][j] = uniquePathHelper(i+1, j, m, n, dp); // move down
-        }
-        return dp[i][j+1] + dp[i+1][j];
+        return dp[0][0];
     }
 }
