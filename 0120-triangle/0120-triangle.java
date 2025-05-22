0a65cf3a013ef93ef0e6b3fc925da9e31b6f50a6
Sumit yogi <yogisumit97@gmail.com>
2025-05-22 16:28:47 +0530

    Time: 3 ms (64.05%), Space: 44.5 MB (28.62%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp.length; j++) dp[i][j] = Integer.MIN_VALUE;
+        int len = triangle.size();
+        int[][] dp = new int[len][len];
+        for(int i=len-1; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                if(i == len-1) dp[i][j] = triangle.get(i).get(j);
+                else{
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+                }
+            }
         } 
-        return minTotalHelper(0, 0, triangle, dp);
-    }
-    private int minTotalHelper(int i, int j, List<List<Integer>> t, int[][] dp){
-        if(i==t.size()) return 0;
-        if(dp[i][j] !=Integer.MIN_VALUE) return dp[i][j];
-        return dp[i][j] = t.get(i).get(j) + Math.min(minTotalHelper(i+1, j, t, dp),minTotalHelper(i+1, j+1, t, dp));        
+        return dp[0][0];
     }
 }
