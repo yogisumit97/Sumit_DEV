3273bd6bb59a8bf1ec1157453893df13cbf67be8
Sumit yogi <yogisumit97@gmail.com>
2025-07-06 12:19:29 +0530

    Time: 0 ms (100%), Space: 41.1 MB (42.29%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,26 +1,18 @@
 class Solution {
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        int[] arr = {1,2,3,4,5,6,7,8,9}; //n =target, k = no. of elements from arr so that sum reaches target
-        int min = 0;
-        int max = 0;
-        for(int i =0;i<k;i++){
-            min+=arr[i];
-            max+=arr[arr.length-i-1];
-        }
-        List<List<Integer>> ans = new ArrayList<>();
-        if(min>n || max<n) return ans;
-        findCombinationsHelper(0,arr,0,n,k,new ArrayList<Integer>(),ans);
-        return ans;
+    public List<List<Integer>> combinationSum3(int k, int tar) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        List<List<Integer>> list = new ArrayList<>();
+        cS3Helper(arr, 0, tar, new ArrayList<Integer>(), list, k);
+        return list;  
     }
-    private void findCombinationsHelper(int index, int[] arr,int sum,int target,int maxElements, List<Integer> result,List<List<Integer>> ans){
-        if(index>=arr.length || sum>=target || result.size()>=maxElements){
-            if(sum==target && result.size()==maxElements) ans.add(new ArrayList<>(result));
+    private void cS3Helper(int[] arr, int i, int tar,List<Integer> res,List<List<Integer>> list, int k){
+        if(i>=arr.length || res.size()==k){
+            if(tar==0 && res.size()==k) list.add(new ArrayList<>(res));
             return;
         }
-        //take or not take method
-        result.add(arr[index]);
-        findCombinationsHelper(index+1,arr,sum+arr[index],target,maxElements,result,ans);
-        result.remove(result.size()-1);
-        findCombinationsHelper(index+1,arr,sum,target,maxElements,result,ans);
+        res.add(arr[i]);
+        cS3Helper(arr, i+1, tar-arr[i], res, list, k);
+        res.remove(res.size()-1);
+        cS3Helper(arr, i+1, tar, res, list, k);
     }
 }
