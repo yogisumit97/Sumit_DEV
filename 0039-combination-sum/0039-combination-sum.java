a8dbafe59de228d096e2f9a277bbbe09a52bfcc3
Sumit yogi <yogisumit97@gmail.com>
2025-07-06 10:17:02 +0530

    Time: 2 ms (83%), Space: 44.7 MB (64.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        generateCombinationsHelper(0,candidates,target,0, new ArrayList<Integer>(), ans);
-        return ans;
+        List<List<Integer>> list = new ArrayList<>();
+        sumHelper(candidates, 0, target, 0, new ArrayList<Integer>(), list);
+        return list;
     }
-    public void generateCombinationsHelper(int index,int[] arr, int target,int sum, List<Integer> result, List<List<Integer>> ans){
-        if(index>=arr.length || sum>=target){
-            if(sum==target) ans.add(new ArrayList<Integer>(result));    
+    private void sumHelper(int[] arr, int i, int tar, int sum, List<Integer> res, List<List<Integer>> list){
+        if(i>=arr.length){
+            if(sum==tar){
+                list.add(new ArrayList<>(res));
+            }
             return;
         }
-        result.add(arr[index]);//take element
-        generateCombinationsHelper(index,arr,target,sum+arr[index], result, ans);
-        result.remove(result.size()-1);//not take element
-        generateCombinationsHelper(index+1,arr,target,sum, result, ans);
-        return;
+        if(sum>tar) return;
+        res.add(arr[i]);
+        sumHelper(arr, i, tar, sum+arr[i],res, list); //take
+        res.remove(res.size()-1);
+        sumHelper(arr, i+1, tar, sum, res, list);
     }
 }
