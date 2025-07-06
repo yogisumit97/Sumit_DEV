4ac99eebb788c68639630ea2f818e692017b4c43
Sumit yogi <yogisumit97@gmail.com>
2025-07-06 21:43:48 +0530

    Time: 148 ms (46.96%), Space: 42 MB (29.62%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean wordFound = false;
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(board[i][j]==word.charAt(0)){
-                    wordFound = wordFound||wordSearchHelper(i,j, board.length, board[0].length, board, word, 0);
-                    if(wordFound) return wordFound;
-                }
+                boolean[][] used = new boolean[board.length][board[0].length];
+                if(helper(board, i, j, word, 0, used)) return true;
             }
         }
         return false;
     }
-    private boolean wordSearchHelper(int i, int j, int row, int col, char[][] board, String word, int index){
-        if(index == word.length()) return true;
-        if(i<0||j<0||i>=row||j>= col||board[i][j]!=word.charAt(index)) return false;
-        char temp = board[i][j];
-        board[i][j] = '-';
-        if(wordSearchHelper(i, j-1, row, col, board, word, index+1)) return true; //go left
-        if(wordSearchHelper(i, j+1, row, col, board, word, index+1)) return true; // go right
-        if(wordSearchHelper(i-1, j, row, col, board, word, index+1)) return true; // go up
-        if(wordSearchHelper(i+1, j, row, col, board, word, index+1)) return true; // go down
-        board[i][j] = temp;
+    private boolean helper(char[][] board, int i, int j, String word, int index, boolean[][] used){
+        if(index>=word.length()) return true;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length) return false;
+        if(used[i][j]){
+            return false;
+        }
+        if(board[i][j]!=word.charAt(index)) return false;
+        // if matched : try U D L R with backtracking 
+        used[i][j] = true;
+        if(helper(board, i-1, j, word, index+1, used)) return true;
+        if(helper(board, i+1, j, word, index+1, used)) return true;
+        if(helper(board, i, j-1, word, index+1, used)) return true;
+        if(helper(board, i, j+1, word, index+1, used)) return true;
+        used[i][j] = false;
         return false;
     }
 }
-
