822089626b0580489eccba371fed244e235a11a1
Sumit yogi <yogisumit97@gmail.com>
2025-07-06 21:17:00 +0530

    Time: 9 ms (25.41%), Space: 56.2 MB (91.88%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        palindromePartitionHelper(0, s,new ArrayList<String>(), ans);
-        return ans;
+        List<List<String>> list = new ArrayList<>();
+        partitionHelper(s, 0, new ArrayList<String>(), list);
+        return list;
     }
-    private void palindromePartitionHelper(int index, String s, List<String> result, List<List<String>> ans){
+    private void partitionHelper(String s, int index, List<String> result, List<List<String>> list){
         if(index>=s.length()){
-            ans.add(new ArrayList<>(result));
+            list.add(new ArrayList<>(result));
             return;
         }
-        for(int i =index+1; i<=s.length();i++){
-            String left_partition = s.substring(index,i);
-            if(checkPalindrome(left_partition)){
-                result.add(left_partition);
-                palindromePartitionHelper(i, s, result, ans);
+        for(int i=index+1; i<=s.length(); i++){
+            if(palindromeCheck(s.substring(index, i))){
+                result.add(s.substring(index,i));
+                partitionHelper(s,i,result,list);
                 result.remove(result.size()-1);
-            } 
+            }
         }
     }
-    private boolean checkPalindrome(String s){
+    private boolean palindromeCheck(String s){
         int left = 0;
         int right = s.length()-1;
         while(left<right){
