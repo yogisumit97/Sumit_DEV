1bad894e66924443794398e0903000cb15717a94
Sumit yogi <yogisumit97@gmail.com>
2025-07-11 15:01:32 +0530

    Time: 16 ms (57.88%), Space: 45.1 MB (34.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,26 @@
 class Solution {
+    public static final int INF = (int)1e8;
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
         int[][] dp = new int[coins.length][amount+1];
-        int result = helper(coins, amount, coins.length-1, dp);
-        return result==1e8 ? -1 : result;
-    }
-    private int helper(int[] coins, int amt, int i, int[][] dp){
-        if(i<0 || amt<0) return (int)1e8;
-        if(amt==0) return 0;
-        if(dp[i][amt]!=0) return dp[i][amt];
-        // take current 
-        int take = 1 + helper(coins, amt-coins[i], i, dp);
-        // not take current
-        int notTake = helper(coins, amt, i-1, dp);
-        return dp[i][amt] = Math.min(take, notTake);
+        for(int i=0; i<coins.length; i++) dp[i][0] = 0;
+        int count = 1;
+        for (int amt = 1; amt <= amount; amt++) {
+            if (amt % coins[0] == 0)
+                dp[0][amt] = amt / coins[0];
+            else
+                dp[0][amt] = INF;
+        }
+        for(int i =1; i<coins.length; i++){
+            for(int amt=0; amt<=amount; amt++){
+                // take current 
+                int take = INF;
+                if(amt>=coins[i]) take = 1 + dp[i][amt-coins[i]];
+                // not take current
+                int notTake = dp[i-1][amt];
+                dp[i][amt] = Math.min(take, notTake);
+            }
+        }
+        return dp[coins.length-1][amount]==INF ? -1 : dp[coins.length-1][amount];
     }
 }
