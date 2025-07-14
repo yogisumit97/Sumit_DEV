2266e7504bb32adb56d3fbde0d747b1a0eefa7e8
Sumit yogi <yogisumit97@gmail.com>
2025-07-14 10:47:32 +0530

    Time: 61 ms (21.47%), Space: 55.5 MB (31.87%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,16 +1,32 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int len = s.length();
-        int[][] dp = new int[len][len];
-        return helper(s, 0, len-1, dp);
-    }
-    private int helper(String s, int start, int end, int[][] dp){
-        if(start==end) return 1;
-        if(start>end) return 0;
-        if(dp[start][end]!=0) return dp[start][end];
-        if(s.charAt(start)==s.charAt(end)) return dp[start][end] = 2 + helper(s, start+1, end-1, dp);
-        else{
-            return dp[start][end] = Math.max(helper(s,start+1, end, dp), helper(s, start, end-1, dp));
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1][len2];
+        if(text1.charAt(0)==text2.charAt(0)) dp[0][0] = 1;
+        for (int j = 1; j < len2; j++) {
+            if (text1.charAt(0) == text2.charAt(j)) dp[0][j] = 1;
+            else dp[0][j] = dp[0][j-1];
         }
+        for (int i = 1; i < len1; i++) {
+            if (text1.charAt(i) == text2.charAt(0)) dp[i][0] = 1;
+            else dp[i][0] = dp[i-1][0];
+        }
+        for(int i1 = 1; i1<len1; i1++){
+            for(int i2=1; i2<len2; i2++){
+                int match = 0;
+                int notMatch = 0;
+                if(text1.charAt(i1)== text2.charAt(i2)) match = 1 + dp[i1-1][i2-1];
+                else notMatch = Math.max(dp[i1-1][i2], dp[i1][i2-1]);
+                dp[i1][i2] = Math.max(match, notMatch);
+            }
+        }
+        return dp[len1-1][len2-1];
+    }
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        return longestCommonSubsequence(s, sb.toString());
     }
 }
