1a383591fc402ac45a0e20575d167af53cbdedb1
Sumit yogi <yogisumit97@gmail.com>
2025-07-15 10:35:14 +0530

    Time: 547 ms (5.24%), Space: 46.9 MB (6.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,21 +2,27 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int len1 = word1.length();
         int len2 = word2.length();
-        int[][] dp = new int[len1][len2];
-        return mdHelper(word1, word2, len1-1, len2-1, dp);
-    }
-    private int mdHelper(String s1, String s2, int i1, int i2, int[][] dp){
-        if(i2<0) return i1+1;
-        if(i1<0) return i2+1;
-        if(dp[i1][i2]!=0) return dp[i1][i2];
-        // last character matching 
-        int match = 0;
-        if(s1.charAt(i1)==s2.charAt(i2)) return mdHelper(s1, s2, i1-1, i2-1, dp);
-        else{ // nonMatching cases
-            int insert = 1 + mdHelper(s1, s2, i1, i2-1, dp);
-            int delete = 1 + mdHelper(s1, s2, i1-1, i2, dp);
-            int replace = 1 + mdHelper(s1, s2, i1-1, i2-1, dp);
-            return dp[i1][i2] = Math.min(insert, Math.min(delete, replace));
+        int[][] dp = new int[len1+1][len2+1];
+        for(int i=1; i<=len1; i++) dp[i][0] = i;
+        for(int i=1; i<=len2; i++) dp[0][i] = i;
+        for(int i=1; i<=len1; i++){
+            for(int j=1; j<=len2; j++){
+                int match = 0;
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else{ // nonMatching cases
+                    int insert = 1 + dp[i][j-1];
+                    int delete = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = Math.min(insert, Math.min(delete, replace));
+                }
+            }
         }
+        for(int i=0; i<=len1; i++){
+            for(int j=0; j<=len2; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return dp[len1][len2];
     }
 }
