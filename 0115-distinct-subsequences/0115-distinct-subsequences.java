6d0b08ca5d3d04c0d9b2c32255093fdc1349f569
Sumit yogi <yogisumit97@gmail.com>
2025-07-15 09:57:15 +0530

    Time: 20 ms (41.01%), Space: 51.3 MB (54.8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,18 +1,17 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()][t.length()];
-        for(int i=0; i<s.length(); i++) Arrays.fill(dp[i], -1);
-        return helper(s,t, 0,0, dp);
-    }
-    private int helper(String s, String t, int i, int j, int[][] dp){
-        if(j==t.length()) return 1;
-        if(i==s.length()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int count = 0;
-        if(s.charAt(i)==t.charAt(j)){
-            count+= helper(s, t, i+1, j+1, dp);
-        } 
-        count+= helper(s, t, i+1, j, dp);
-        return dp[i][j] = count;
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for(int i=0; i<dp.length; i++) dp[i][t.length()] = 1;
+        for(int i=s.length()-1; i>=0; i--){
+            for(int j=t.length()-1; j>=0; j--){
+                int count = 0;
+                if(s.charAt(i)==t.charAt(j)){
+                    count+= dp[i+1][j+1];
+                } 
+                count+= dp[i+1][j];
+                dp[i][j] = count;
+            }
+        }
+        return dp[0][0];
     }
 }
