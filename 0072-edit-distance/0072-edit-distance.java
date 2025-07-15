f6beb00dc47a0c645e9e1bc6af48011afed37a10
Sumit yogi <yogisumit97@gmail.com>
2025-07-15 10:22:15 +0530

    Time: 4 ms (72.41%), Space: 45.1 MB (46.1%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()][word2.length()];
+        return mdHelper(word1, word2, word1.length()-1, word2.length()-1, dp);
+    }
+    private int mdHelper(String s1, String s2, int i1, int i2, int[][] dp){
+        if(i2<0) return i1+1;
+        if(i1<0) return i2+1;
+        if(dp[i1][i2]!=0) return dp[i1][i2];
+        // last character matching 
+        int match = 0;
+        if(s1.charAt(i1)==s2.charAt(i2)) return mdHelper(s1, s2, i1-1, i2-1, dp);
+        else{ // nonMatching cases
+            int insert = 1 + mdHelper(s1, s2, i1, i2-1, dp);
+            int delete = 1 + mdHelper(s1, s2, i1-1, i2, dp);
+            int replace = 1 + mdHelper(s1, s2, i1-1, i2-1, dp);
+            return dp[i1][i2] = Math.min(insert, Math.min(delete, replace));
+        }
+    }
+}
