d9a6017854faf7f063e861461454f1c2a9ec401d
Sumit yogi <yogisumit97@gmail.com>
2025-07-22 19:01:30 +0530

    Time: 1 ms (100%), Space: 42.2 MB (61.92%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return helper(s, p, s.length()-1, p.length()-1);
+        Boolean[][] dp = new Boolean[s.length()][p.length()];
+        return helper(s, p, s.length()-1, p.length()-1, dp);
     }
-    private boolean helper(String s, String p, int i1, int i2){
+    private boolean helper(String s, String p, int i1, int i2, Boolean[][] dp){
         if(i1==-1 && i2<=-1) return true;
         if(i1==-1){
             while(i2>=0 && p.charAt(i2)=='*') i2=i2-2;
@@ -10,15 +11,15 @@ class Solution {
             return false;
         }
         if(i2<=-1) return false;
-
+        if(dp[i1][i2]!=null) return dp[i1][i2];
         if(s.charAt(i1)== p.charAt(i2) || p.charAt(i2)=='.'){
-            return helper(s,p,i1-1, i2-1);
+            return dp[i1][i2] = helper(s,p,i1-1, i2-1, dp);
         }
         else if(p.charAt(i2)=='*'){
             boolean check = false; // check if prev matches  
-            if(i1>=0 && i2-1>= 0 && (s.charAt(i1) == p.charAt(i2-1)|| p.charAt(i2-1)=='.')) check = helper(s, p, i1-1, i2);
-            return helper(s, p, i1, i2-2)| check;
+            if(i1>=0 && i2-1>= 0 && (s.charAt(i1) == p.charAt(i2-1)|| p.charAt(i2-1)=='.')) check = helper(s, p, i1-1, i2, dp);
+            return dp[i1][i2] = helper(s, p, i1, i2-2, dp)| check;
         }
-        else return false;
+        else return dp[i1][i2] = false;
     }
 }
