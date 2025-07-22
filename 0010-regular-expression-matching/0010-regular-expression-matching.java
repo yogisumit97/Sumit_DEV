46f665e3e8b5d4f22de82f0dddbc7b5a8dcf9c8f
Sumit yogi <yogisumit97@gmail.com>
2025-07-22 18:59:38 +0530

    Time: 12 ms (26.37%), Space: 41.9 MB (86.69%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return helper(s, p, s.length()-1, p.length()-1);
+    }
+    private boolean helper(String s, String p, int i1, int i2){
+        if(i1==-1 && i2<=-1) return true;
+        if(i1==-1){
+            while(i2>=0 && p.charAt(i2)=='*') i2=i2-2;
+            if(i2<=-1) return true;
+            return false;
+        }
+        if(i2<=-1) return false;
+
+        if(s.charAt(i1)== p.charAt(i2) || p.charAt(i2)=='.'){
+            return helper(s,p,i1-1, i2-1);
+        }
+        else if(p.charAt(i2)=='*'){
+            boolean check = false; // check if prev matches  
+            if(i1>=0 && i2-1>= 0 && (s.charAt(i1) == p.charAt(i2-1)|| p.charAt(i2-1)=='.')) check = helper(s, p, i1-1, i2);
+            return helper(s, p, i1, i2-2)| check;
+        }
+        else return false;
+    }
+}
