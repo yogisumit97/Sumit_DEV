d0ed0bbdb5ad6fc2098ce31784d59a4608d7cd0e
Sumit yogi <yogisumit97@gmail.com>
2025-07-24 08:01:10 +0530

    Time: 2 ms (83.88%), Space: 43.9 MB (57.33%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,28 +1,21 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int len = prices.length;
-        int[][][] dp = new int[len][2][k+1];
-        for(int i=0; i<len; i++){
+        int[][][] dp = new int[len+1][2][k+1];
+        for(int i=len-1; i>=0; i--){
             for(int j=0; j<2; j++){
-                for(int p=0; p<=k; p++){
-                    dp[i][j][p] = -1;
+                for(int cap=1; cap<=k; cap++){
+                    int profit = 0;
+                    if(j==1){
+                        profit = Math.max(-1*prices[i]+ dp[i+1][0][cap], dp[i+1][1][cap]);// buy or skip
+                    }
+                    else{
+                        profit = Math.max(prices[i]+ dp[i+1][1][cap-1], dp[i+1][0][cap]);// sell or skip
+                    }
+                    dp[i][j][cap] = profit;
                 }
             }
         }
-        return helper(prices, 0, 1, k, dp);
-    }
-    private int helper(int[] prices, int day, int canBuy, int cap, int[][][] dp){
-        if(day>=prices.length || cap==0){
-            return 0;
-        }
-        if(dp[day][canBuy][cap]!=-1) return dp[day][canBuy][cap]; 
-        int profit = 0;
-        if(canBuy==1){
-            profit = Math.max(-1*prices[day]+ helper(prices, day+1, 0, cap, dp), helper(prices, day+1, 1, cap, dp)); // buy or skip
-        }
-        else{
-            profit = Math.max(prices[day]+ helper(prices, day+1, 1, cap-1, dp), helper(prices, day+1, 0, cap, dp)); // sell or skip
-        }
-        return dp[day][canBuy][cap] = profit;
+        return dp[0][1][k];
     }
 }
