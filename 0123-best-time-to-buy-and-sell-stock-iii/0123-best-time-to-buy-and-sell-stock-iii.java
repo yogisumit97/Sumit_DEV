e8df60190877c2b2fcb2707a4c80124b23fb755e
Sumit yogi <yogisumit97@gmail.com>
2025-07-24 07:51:03 +0530

    Time: 62 ms (39.25%), Space: 63.6 MB (42.54%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,27 +1,46 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][][] dp = new int[prices.length][2][2];
-        for(int i=0; i<prices.length; i++){
-            for(int j=0; j<2; j++){
-                for(int k=0; k<2; k++) dp[i][j][k] = -1;
+        int len = prices.length;
+        int[][][] dp = new int[len+1][2][3];
+        for(int i=0;i<len; i++){
+            for(int j =0; j<2; j++){
+                dp[i][j][0] = 0; // cap = 0 base case
             }
         }
-        return helper(prices, 0, true, dp, 2);
+        for(int j=0;j<2; j++){
+            for(int k =0; k<3; k++){
+                dp[len][j][k] = 0; // day=len base case
+            }
+        }
+
+        for(int i=len-1;i>=0; i--){
+            for(int j =0; j<2; j++){
+                for(int k=0; k<3; k++){
+                    int profit = 0;
+                    if(j==1){
+                        profit = Math.max(-1*prices[i]+ dp[i+1][0][k], dp[i+1][1][k]); // buy or skip
+                    }
+                    else{
+                        if(k>0) profit = Math.max(prices[i]+ dp[i+1][1][k-1], dp[i+1][0][k]); // sell or skip
+                    }
+                    dp[i][j][k] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
-    private int helper(int[] prices, int i, boolean canBuy, int[][][] dp, int rem){
-        if(i>=prices.length || rem==0) return 0;
-        int buy = 0;
-        int skip = 0;
-        int sell = 0;
-        int j = canBuy==true? 1 : 0;
-        if(dp[i][j][rem-1] !=-1) return dp[i][j][rem-1];
-        if(canBuy){
-            buy = -1*prices[i] + helper(prices, i+1, false, dp, rem);
+    private int helper(int[] prices, int day, int canBuy, int cap, int[][][] dp){
+        if(day>=prices.length || cap==0){
+            return 0;
         }
-        if(!canBuy){
-            sell = prices[i] + helper(prices, i+1, true, dp, rem-1);
+        if(dp[day][canBuy][cap]!=-1) return dp[day][canBuy][cap];
+        int profit = 0;
+        if(canBuy==1){
+            profit = Math.max(-1*prices[day]+ helper(prices, day+1, 0, cap, dp), helper(prices, day+1, 1, cap, dp)); // buy or skip
         }
-        skip = helper(prices, i+1, canBuy, dp, rem);
-        return dp[i][j][rem-1] = Math.max(skip, Math.max(buy, sell));
+        else{
+            profit = Math.max(prices[day]+ helper(prices, day+1, 1, cap-1, dp), helper(prices, day+1, 0, cap, dp)); // sell or skip
+        }
+        return dp[day][canBuy][cap] = profit;
     }
 }
