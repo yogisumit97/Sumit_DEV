1b8da0183981737db9b66598a693ced88194a776
Sumit yogi <yogisumit97@gmail.com>
2025-07-24 06:59:45 +0530

    Time: 4 ms (6.03%), Space: 45.8 MB (59.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for(int i=0; i<prices.length; i++) dp[i][0] = dp[i][1] = -1;
-        return helper(prices, 0, true, dp);
-    }
-    private int helper(int[] prices, int i, boolean canBuy, int[][] dp){
-        if(i>=prices.length) return 0;
-        int buy = 0;
-        int skip = 0;
-        int sell = 0;
-        int j = canBuy==true? 1 : 0;
-        if(dp[i][j] !=-1) return dp[i][j];
-        if(canBuy){
-            buy = -1*prices[i] + helper(prices, i+1, false, dp);
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -1*prices[0];
+        for(int i = 1; i< len; i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0]-prices[i]);
         }
-        if(!canBuy){
-            sell = prices[i] + helper(prices, i+1, true, dp);
-        }
-        skip = helper(prices, i+1, canBuy, dp);
-        return dp[i][j] = Math.max(skip, Math.max(buy, sell));
+        return dp[len-1][0];
     }
 }
