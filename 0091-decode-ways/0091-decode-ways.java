7c2789470750a44f864a8d1f76d5efb8b1efb2ca
Sumit yogi <yogisumit97@gmail.com>
2025-07-26 08:50:53 +0530

    Time: 1 ms (82.92%), Space: 41.3 MB (98.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,15 +1,17 @@
 class Solution {
     public int numDecodings(String s) {
-        int[][] dp = new int[s.length()][10];
-        return helper(s, 0, 0, dp);
-    }
-    private int helper(String s, int i, int prev, int[][] dp){
-        if(i==s.length()) return 1;
-        if(dp[i][prev] !=0) return dp[i][prev];
-        int curr = Character.getNumericValue(s.charAt(i));
-        int ans = 0;
-        if(curr!=0) ans+= helper(s, i+1, curr, dp);
-        if(prev!=0 && (prev*10+curr)!=0 && (prev*10+curr)<=26) ans+= helper(s, i+1, 0, dp);
-        return dp[i][prev] = ans; 
+        int len = s.length();
+        int[][] dp = new int[len+1][10];
+        for(int prev =0; prev<10; prev++) dp[len][prev] = 1;
+        for(int i=s.length()-1; i>=0; i--){
+            for(int prev=0; prev<10; prev++){
+                int curr = Character.getNumericValue(s.charAt(i));
+                int ans = 0;
+                if(curr!=0) ans+= dp[i+1][curr];
+                if(prev!=0 && (prev*10+curr)!=0 && (prev*10+curr)<=26) ans+= dp[i+1][0];
+                dp[i][prev] = ans;
+            }
+        }
+        return dp[0][0];
     }
 }
