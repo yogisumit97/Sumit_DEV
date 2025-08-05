3b406722daf1699f4a8d33c86e374e8fd13b5d3d
Sumit yogi <yogisumit97@gmail.com>
2025-08-05 16:43:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // brute force.
-        int one= 0;
-        int two= 0;
-        int zero = 0;
-        for(int i : nums){
-            if(i==0) zero++;
-            else if(i==1) one++;
-            else two++;
-        }
-        int i=0;
-        while(zero!=0){
-            nums[i++] = 0;
-            zero--;
-        }
-        while(one!=0){
-            nums[i++] = 1;
-            one--;
-        }
-        while(two!=0){
-            nums[i++] = 2;
-            two--;
-        }
+        // assumption 0 to low-1 all 0, low to mid-1 all 1, high+1 to n all 2
+        // so mid  to high are all random numbers
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0 :
+                    swap(nums, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1 :
+                    mid++;
+                    break;
+                case 2 :
+                    swap(nums, mid, high);
+                    high--;
+                    break;
+            }
+        }    
+    }
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
