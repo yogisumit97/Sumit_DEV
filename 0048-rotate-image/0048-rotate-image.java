40780385c837bbb422882147914f10f82aab3364
Sumit yogi <yogisumit97@gmail.com>
2025-08-06 12:09:30 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -12,12 +12,6 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
     }
     public void reverseMatrix(int[][] matrix){
         int left = 0;
