01b3b34113bf1fbf805b4e299551c9639bba461f
Sumit yogi <yogisumit97@gmail.com>
2025-08-08 10:27:07 +0530

    Time: 1 ms (26.03%), Space: 41.6 MB (64.68%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -1,14 +1,17 @@
 class Solution {
     public int maxDepth(String s) {
-        int max_overall = 0;  //kadane's algorithm types implementation in finding max
-        int max_current = 0;
-        for(char i : s.toCharArray()){
-            if(i=='('){
-                max_current++;
-                max_overall = Math.max(max_overall,max_current);
+        int max = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch== '('){
+                stack.push(ch);
+                max = Math.max(max, stack.size());
             }
-            else if(i==')') max_current = Math.max(max_current-1,0);
+            else if(ch==')'){
+                stack.pop();
+            }
         }
-        return max_overall;
+        return max;
     }
 }
