5758e66c996395c7c0d3950a191d60c7a0a11ce6
Sumit yogi <yogisumit97@gmail.com>
2025-08-13 13:29:13 +0530

    Time: 6 ms (61.48%), Space: 44.6 MB (48.79%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // dp solution 
-        int maxLen = Integer.MIN_VALUE; // if substring is greater than this do not check further
-        HashSet<String> dictSet = new HashSet<>();
-        for(String i : wordDict){
-            dictSet.add(i);
-            maxLen = Math.max(maxLen,i.length());
-        }
-        boolean[] isSegmentPossible = new boolean[s.length()+1]; //dp array : keeps track if substring(0,i+1) is possible or not using this dictionary
-        isSegmentPossible[0]=true; // if nothing is taken segment is possible;
-        for(int i =0 ; i<s.length(); i++){ // iterate over string
-            int count = 0;
-            for(int j=i; j>=0 && count!=maxLen; j--){
-                if(checkDictionary(s.substring(j,i+1),dictSet)){
-                    isSegmentPossible[i+1] =isSegmentPossible[i+1] || isSegmentPossible[j]; 
-                }
-                count++;
-            }
-        }
-        return isSegmentPossible[s.length()];
+        HashSet<String> dict = new HashSet<>();
+        Boolean[][] dp = new Boolean[s.length()+1][s.length()+1];
+        for(String str : wordDict) dict.add(str);
+        return checkWordInDict(s, 0, 1, dict, dp);
     }
-    public boolean checkDictionary(String s, HashSet<String> dict){
-        if(dict.contains(s)) return true;
-        return false;
+    private boolean checkWordInDict(String s, int start, int end, HashSet<String> dict, Boolean[][] dp){
+        if(start>=s.length()) return true;
+        if(end>=s.length()){
+            return dict.contains(s.substring(start, s.length()));
+        }
+        if(dp[start][end]!=null) return dp[start][end];
+        boolean wordExists = false;
+        if(dict.contains(s.substring(start, end))) wordExists|= checkWordInDict(s, end, end+1, dict, dp);
+        if(wordExists) return dp[start][end] = true;
+        wordExists|= checkWordInDict(s, start, end+1, dict, dp);
+        return dp[start][end] = wordExists;
     }
 }
