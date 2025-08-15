e8d40852d91e68eed920471ba09874fa88471bbf
Sumit yogi <yogisumit97@gmail.com>
2025-08-15 08:57:10 +0530

    Time: 1 ms (6.77%), Space: 40.9 MB (16.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,12 @@
-class Solution { // using combinatorics  ?? need to learn
+class Solution {
     public int uniquePaths(int m, int n) {
-        int N = n+m-2;
-        int r = m - 1;
-        double res = 1;
-        for(int i=1; i<=r; i++){
-            res = res *(N - r + i)/i;
-        }
-        return (int)res;
+        int[][] dp = new int[m][n];
+        return helper(0,0, m-1, n-1, dp);
+    }
+    private int helper(int i, int j, int m, int n, int[][] dp){
+        if(i==m && j==n) return 1;
+        if(i>m || j>n) return 0;
+        if(dp[i][j] !=0) return dp[i][j];
+        return dp[i][j] = helper(i+1, j, m, n, dp)+ helper(i, j+1, m, n, dp);
     }
 }
