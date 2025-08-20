4e31168abc095cb915b0c13fce54aa5709e2879e
Sumit yogi <yogisumit97@gmail.com>
2025-08-20 08:25:28 +0530

    Time: 96 ms (16.8%), Space: 54.8 MB (10.45%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int[][] dp = new int[words.length+1][words.length+1];
+        return helper(0, words, words.length, dp);
+    }
+    private int helper(int i, String[] words, int prevIdx, int[][] dp){
+        if(i>= words.length){
+            return 0;
+        }
+        if(dp[i][prevIdx]!=0) return dp[i][prevIdx];
+        // take and skip method
+        int take = 0;
+        if(prevIdx ==words.length || (words[i].length() == words[prevIdx].length()+1 && isSubsequence(words, i, prevIdx))){
+            take = 1 + helper(i+1, words, i, dp);
+        }
+        int skip = helper(i+1, words, prevIdx, dp);
+        return dp[i][prevIdx] = Math.max(take, skip);
+    }
+    private boolean isSubsequence(String[] words, int i, int j){
+        int ind1 = 0;
+        int ind2 = 0;
+        while(ind1 < words[i].length() && ind2 < words[j].length()){
+            if(words[i].charAt(ind1) == words[j].charAt(ind2)){
+                ind1++;
+                ind2++;
+            }
+            else ind1++;
+        }
+
+        if(ind2 == words[j].length()) return true;
+        return false;
+    } 
+}
