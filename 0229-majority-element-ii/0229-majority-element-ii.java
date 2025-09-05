96b03ab592561f96ea5bc6da1543e08bc39f01ab
Sumit yogi <yogisumit97@gmail.com>
2025-09-05 08:47:30 +0530

    Time: 2 ms (99.79%), Space: 50.6 MB (26.96%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int min = nums.length/3;
         List<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int el1 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int el2 = Integer.MIN_VALUE;
+        int cnt2 = 0;
         for(int num : nums){
-            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
-            if(freqMap.get(num) > min){
-                if(list.size()==0) list.add(num);
-                else if(list.size()==1 && list.get(0)!= num) list.add(num);
-                else if(list.size()==2) return list;
-                else continue;
+            if(cnt1==0 && num !=el2){
+                cnt1 = 1;
+                el1 = num;
+            }
+            else if(cnt2==0 && num!= el1){
+                cnt2 = 1;
+                el2 = num;
+            }
+            else if(num==el1) cnt1++;
+            else if(num==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1 = cnt2 = 0;
+        for(int num : nums){
+            if(num==el1) cnt1++;
+            if(num==el2) cnt2++;
+        }
+        if(cnt1 > nums.length/3) list.add(el1);
+        if(cnt2 > nums.length/3) list.add(el2);
+        
         return list;
-    }    
+    }   
 }
