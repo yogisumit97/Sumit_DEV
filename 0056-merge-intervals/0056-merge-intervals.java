f4dcf2b5eacc69cbfc617ec653ba1077d869fb18
Sumit yogi <yogisumit97@gmail.com>
2025-09-21 08:52:17 +0530

    Time: 11 ms (11.26%), Space: 46.6 MB (73.94%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,33 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-    List<int[]> list = new ArrayList<>();
-    for (int[] interval : intervals) {
-        if (list.isEmpty() || list.get(list.size() - 1)[1] < interval[0]) {
-            list.add(interval);  // No overlap
-        } else {
-            // Overlap: merge by updating the end
-            list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], interval[1]);
+    class Pair {
+        int start;
+        int end;
+        Pair(int i, int j){
+            start = i;
+            end = j;
         }
     }
-    return list.toArray(new int[list.size()][]);
-}
-
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+        List<Pair> list = new ArrayList<>();
+        for(int i =0; i<intervals.length; i++){
+            if(list.size()==0 || (intervals[i][0] > list.get(list.size()-1).end)){
+                list.add(new Pair(intervals[i][0], intervals[i][1]));
+            }
+            else{
+                Pair temp = list.get(list.size()-1);
+                list.remove(list.size()-1);
+                temp.end = Math.max(temp.end, intervals[i][1]);
+                list.add(temp);
+            }
+        }
+        int[][] mergedIntervals = new int[list.size()][2];
+        int i = 0;
+        for(Pair p : list){
+            mergedIntervals[i][0] = p.start;
+            mergedIntervals[i][1] = p.end;
+            i++;
+        }
+        return mergedIntervals;
+    }
 }
