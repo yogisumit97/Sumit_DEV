aa7320196af397406fb1203560a63b7fda1f187c
Sumit yogi <yogisumit97@gmail.com>
2025-11-13 09:30:50 +0530

    Time: 0 ms (100%), Space: 46.6 MB (6.61%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,30 +1,18 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        //hare tortoise algo 
         ListNode fast = head;
         ListNode slow = head;
-        do{
-            if(slow == null || slow.next ==null) return null;
-            else if(fast == null || fast.next == null) return null;
-            slow = slow.next;
+        while(fast!=null && fast.next!=null && slow!=null){
             fast = fast.next.next;
-        }while(fast!=slow);
-        slow = head;
-        while(fast!=slow){
             slow = slow.next;
-            fast=fast.next;
+            if(fast==slow) break;
         }
-        return slow;
+        if(fast!=slow || fast ==null || fast.next ==null) return null;
+        fast = head;
+        while(fast!=slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
     }
 }
