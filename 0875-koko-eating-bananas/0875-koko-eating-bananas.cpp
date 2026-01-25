16106d6afde96cfab8147b029275d34348da9d63
Sumit yogi <yogisumit97@gmail.com>
2026-01-25 11:24:39 +0530

    Time: 21 ms (7.14%), Space: 22.9 MB (76.58%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int minSpeed = 1;
+        int maxSpeed = *max_element(piles.begin(), piles.end());
+        int neededSpeed = 0;
+        while(minSpeed<=maxSpeed){
+            int currSpeed = (minSpeed+maxSpeed)/2;
+            long int time = 0;
+            for(int pile : piles){
+                double f = pile*1.0/currSpeed;
+                int i = (int)ceil(f);
+                time+=i;
+            }
+            if(time > h){
+                minSpeed = currSpeed+1;
+            }
+            else{
+                maxSpeed = currSpeed-1;
+            }
+        }
+        return minSpeed;
+    }
+};
